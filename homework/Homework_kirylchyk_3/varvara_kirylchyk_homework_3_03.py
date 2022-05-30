a14a5b01125b1691dd8fb3b813792b1e2bc2bd20
Varvara <kirylchyk@gmail.com>
2022-05-30 15:41:56 -0400

    homework_3_varvara_kirylchyk_done

---
+++ homework/Homework_kirylchyk_3/varvara_kirylchyk_homework_3_03.py
@@ -0,0 +1,11 @@
+#Даны два действительных числа. Найти среднее арифметическое и среднее геометрическое этих чисел
+import math
+
+a=1
+b=2
+print((a+b)/2)
+num=a*b
+geometrical=math.sqrt(num)
+print(geometrical)
+
+
