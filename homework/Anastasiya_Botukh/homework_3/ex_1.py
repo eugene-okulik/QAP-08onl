382a9e26b7ff69b997562c9abe9f06fc714178d4
Anastasiya <aisatsana@MacBook-Air-AISATSANA.local>
2022-06-01 23:05:57 +0300

    Anastasiya_Botukh_Homework_3

---
+++ homework/Anastasiya_Botukh/homework_3/ex_1.py
@@ -0,0 +1,8 @@
+a = 22
+b = 37
+c = a + b
+print(c)
+c = a - b
+print(c)
+c = a * b
+print (c)
---
+++ homework/Anastasiya_Botukh/homework_3/ex_4.py
@@ -0,0 +1,11 @@
+# гипотенуза
+
+import math
+cat_a = 16
+cat_b = 12
+gip = (cat_a ** 2) + (cat_b ** 2)
+print(math.sqrt(gip))
+# площадь
+
+s = (cat_a * cat_b) / int(2)
+print(s)
