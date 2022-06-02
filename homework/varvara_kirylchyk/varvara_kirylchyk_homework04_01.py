a6646a6a6a2cc38e8d3d2b35bed5ccb03b349907
Varvara <kirylchyk@gmail.com>
2022-06-02 16:35:04 -0400

    homework_4_varvara_kirylchyk_task01

---
+++ homework/varvara_kirylchyk/varvara_kirylchyk_homework04_01.py
@@ -0,0 +1,24 @@
+#Напишите программу, которая добавляет ‘ing’ к словам в тексте
+# “Etiam tincidunt neque erat, quis molestie enim imperdiet vel.
+# Integer urna nisl, facilisis vitae semper at, dignissim vitae libero”
+
+MY_STRING = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel.' \
+            ' Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+
+MY_LIST = MY_STRING.split()
+L=[]
+
+for word in MY_LIST:
+    if word[-1] in ".,":
+        new_word = word[:-1]+ 'ing'+word[-2:]
+        L.append(new_word)
+    else:
+        new_word = word + 'ing'
+        L.append(new_word)
+
+MY_NEW_SRTING = ' '.join(L)
+print(MY_NEW_SRTING)
+
+
+
+
