34ccce69860819f7b01fd3a2c2b2e0ec3ea25d66
Artyom Zabello <a.zabela@andersenlab.com>
2022-06-03 00:08:39 +0300

    First task passed

---
+++ homework/artyom_zabello/Homework_4/task1.py
@@ -0,0 +1,14 @@
+TEXT = ('Etiam tincidunt neque erat, quis molestie enim'
+        'imperdiet vel. Integer urna nisl, facilisis vitae semper at,'
+        'dignissim vitae libero')
+
+
+def add_ing(text):
+    text = text.replace(',', "").replace(".", "").split(" ")
+    lst = list()
+    for i in text:
+        lst.append(i + "ing")
+    return print(lst)
+
+
+add_ing(TEXT)
