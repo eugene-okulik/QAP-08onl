681a2ae1b3c35d41f282ae651f807d853691cc34
tmolofei <tmolofei@yandex.by>
2022-06-02 15:54:28 +0300

    Homework_3 done

---
+++ homework/tatiana_molofei/Homework_3/hw_03_03.py
@@ -0,0 +1,10 @@
+# Даны два действительных числа. Найти среднее арифметическое и среднее геометрическое этих чисел
+
+a = 84
+b = 16
+
+average_arif = (a + b) / 2
+print(average_arif)
+
+average_geom = (a * b) ** 0.5
+print(average_geom)
