70d1b731432bd89c92210e2efae448f9e7211c00
Artyom Zabello <a.zabela@andersenlab.com>
2022-06-03 00:37:24 +0300

    Second task passed

--- homework/artyom_zabello/Homework_4/task1.py
+++ homework/artyom_zabello/Homework_4/task1.py
@@ -7,7 +7,7 @@ def add_ing(text):
     text = text.replace(',', "").replace(".", "").split(" ")
     lst = list()
     for i in text:
-        lst.append(i + "ing")
+        lst.append(i+"ing")
     return print(lst)
 
 
