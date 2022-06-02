3f13fce690f940971dfe23acee1dfd362be36422
SiarheiZhachek <SiarheiZhachek@yandex.by>
2022-06-02 03:00:40 +0300

    Homework_3_corrected

---
+++ homework/siarhei_zhachek/Homework_3/cube.py
@@ -0,0 +1,12 @@
+NUM_A = input('What is the length of the edge of the cube? ')
+NUM_A = int(NUM_A)
+NUM_V = NUM_A ** 3
+NUM_S = (NUM_A ** 2) * 4
+RESULT_TEXT_NUM_A = 'The volume of the cube is '
+RESULT_NUM_A = RESULT_TEXT_NUM_A + str(NUM_V)
+RESULT_TEXT_NUM_B = 'The area of the side surface is '
+RESULT_NUM_B = RESULT_TEXT_NUM_B + str(NUM_S)
+print(RESULT_NUM_A)
+print()
+print(RESULT_NUM_B)
+10
