4d0d66d12dae2c7c22b99d2b9447139e017bd5a7
Varvara <kirylchyk@gmail.com>
2022-06-02 17:52:06 -0400

    homework_4_varvara_kirylchyk_task02

--- homework/varvara_kirylchyk/varvara_kirylchyk_homework04_01.py
+++ homework/varvara_kirylchyk/varvara_kirylchyk_homework04_01.py
@@ -6,18 +6,19 @@ MY_STRING = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel.' \
             ' Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
 
 MY_LIST = MY_STRING.split()
-L=[]
+MY_NEW_LIST=[]
 
 for word in MY_LIST:
     if word[-1] in ".,":
         new_word = word[:-1]+ 'ing'+word[-2:]
-        L.append(new_word)
+        MY_NEW_LIST.append(new_word)
     else:
         new_word = word + 'ing'
-        L.append(new_word)
+        MY_NEW_LIST.append(new_word)
 
-MY_NEW_SRTING = ' '.join(L)
+MY_NEW_SRTING = ' '.join(MY_NEW_LIST)
 print(MY_NEW_SRTING)
+
 
 
 
--- homework/varvara_kirylchyk/varvara_kirylchyk_homework04_02.py
+++ homework/varvara_kirylchyk/varvara_kirylchyk_homework04_02.py
@@ -1,6 +1,20 @@
-Есть строка: test_string = "Hello world!",
-необходимо:
-вывести на экран на каком месте находится буква w
-кол-во букв “l”
-Проверить начинается ли строка с подстроки: “Hello”
-Проверить заканчивается ли строка подстрокой: “qwe”
+#Есть строка: test_string = "Hello world!",
+#Необходимо:
+#-вывести на экран на каком месте находится буква w
+#кол-во букв “l”
+#Проверить начинается ли строка с подстроки: “Hello”
+#Проверить заканчивается ли строка подстрокой: “qwe”
+
+
+TEST_STRING = "Hello world!"
+print (TEST_STRING.find("w"))
+
+COUNT = TEST_STRING.count('l')
+print("The count of 'l' is ", COUNT)
+
+if TEST_STRING.startswith ('Hello'):
+    print ('Yes, it starts with "Hello"')
+elif TEST_STRING.startswith ('qwe'):
+    print ('Yes, it starts with "qwe"')
+else:
+    print("No")
