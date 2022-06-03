1eff046170f57ed9ae1debc6958b43b83c43c190
tmolofei <tmolofei@yandex.by>
2022-06-03 10:17:18 +0300

    Added changes in constant name in Homework_4

--- homework/tatiana_molofei/Homework_4/hw_04_01.py
+++ homework/tatiana_molofei/Homework_4/hw_04_01.py
@@ -2,10 +2,10 @@
 # “Etiam tincidunt neque erat, quis molestie enim imperdiet vel.
 # Integer urna nisl, facilisis vitae semper at, dignissim vitae libero”
 
-text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. ' \
+TEXT = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. ' \
        'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
 
-my_list = text.split()
+my_list = TEXT.split()
 my_list_1 = my_list.append(' ')
 
 TEXT_1 = 'ing '.join(my_list)
