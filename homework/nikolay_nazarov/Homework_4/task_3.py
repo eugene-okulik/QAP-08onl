ac0810b0336bec02296324d02503c6d4ac9676b0
Nikolay Nazarov <nikolaynazarovvv216@gmail.com>
2022-06-03 21:12:44 +0300

    homework_4 done

---
+++ homework/nikolay_nazarov/Homework_4/task_3.py
@@ -0,0 +1,11 @@
+# Создайте переменную с текстом “Mauris fringilla odio sit amet pretium ultricies.
+# Pellentesque habitant morbi tristique”. извлеките из нее следующие срезы:
+# - первые восемь символов
+# - четыре символа из центра строки
+# - символы с индексами кратными трем
+
+SOME_TEXT = "Mauris fringilla odio sit amet pretium ultricies. " \
+            "Pellentesque habitant morbi tristique"
+print(SOME_TEXT[:8])
+print(SOME_TEXT[int((len(SOME_TEXT)-1)/2-2):int((len(SOME_TEXT)-1)/2+2)])
+print(SOME_TEXT[4::4])
