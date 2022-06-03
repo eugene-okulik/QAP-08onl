ea4bf4b4e90b00754e73a83b26c076f21a9c9553
andrei.ananich <andreiananich@yandex.by>
2022-06-03 16:15:46 +0300

    Homework_3 done

---
+++ homework/andrei_ananich/homework_3/homework_3_2.py
@@ -0,0 +1,6 @@
+# Даны действительные числа x и y. Получить x − y / 1 + xy
+
+NUM_A = 7
+NUM_B = 5
+print((NUM_A - NUM_B / 1) + NUM_A * NUM_B)
+
---
+++ homework/andrei_ananich/homework_3/homework_3_4.py
@@ -0,0 +1,10 @@
+# Даны катеты прямоугольного треугольника. Найти его гипотенузу и площадь
+
+NUM_A = 6
+NUM_B = 8
+
+hypotenuse = (NUM_A ** 2 + NUM_B ** 2) ** 0.5
+print('Гипотенуза равна', hypotenuse)
+
+NUM_square = (NUM_A * NUM_B) * 0.5
+print('Площадь равна', NUM_square)
