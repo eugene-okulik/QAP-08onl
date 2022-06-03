ef2a2aae4bad96fa64638703bf14aa2f2a3c8520
Vadim Zhenevsky <zhenevskyvadim@gmail.com>
2022-06-03 15:15:24 +0300

    homework_3_done

---
+++ homework/vadim_zhenevsky/homework_3/hw_3_3.py
@@ -0,0 +1,6 @@
+z = 4
+x = 8
+q = (z + x) / 2
+print(q)
+p = (z * x) / 2
+print(p)
---
+++ homework/vadim_zhenevsky/homework_3/hw_3_4.py
@@ -0,0 +1,6 @@
+c = 4
+b = 6
+d = (c ** 2 + b ** 2) ** 0.5
+print('Hypotenuse = ' + str(d))
+k = c * b / 2
+print('Square = ' + str(k))
