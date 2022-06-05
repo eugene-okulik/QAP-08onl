35a166a44946c992a0fa36dc432df73e07889bb0
NikolayNazarov <nikolaynazarovvv216@gmail.com>
2022-06-05 21:32:09 +0300

    Поправил название пул реквеста

--- homework/nikolay_nazarov/Homework_4/task_3.py
+++ homework/nikolay_nazarov/Homework_4/task_3.py
@@ -2,7 +2,7 @@
 # Pellentesque habitant morbi tristique”. извлеките из нее следующие срезы:
 # - первые восемь символов
 # - четыре символа из центра строки
-# - символы с индексами кратными трем
+# - символы с индексами кратными трем.
 
 SOME_TEXT = "Mauris fringilla odio sit amet pretium ultricies. " \
             "Pellentesque habitant morbi tristique"
