f7dbadb2522dc43413d63bf0bd7fe30aaf292d58
Sergey1991wolk <svwolk1991@gmail.com>
2022-06-06 16:41:19 +0300

    workhome

--- homework/wolk_sergey/wolk_sergey_homework_4/homework_4/hwork_1.py
+++ homework/wolk_sergey/wolk_sergey_homework_4/homework_4/hwork_1.py
@@ -2,7 +2,7 @@ TEXT = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. ' \
        'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
 
 TEXT = TEXT.split()
-LIST1 = TEXT.append(' ')
+
 
 LIST1 = 'ing '.join(TEXT)
 LIST2 = LIST1.replace(",ing", "ing,")
