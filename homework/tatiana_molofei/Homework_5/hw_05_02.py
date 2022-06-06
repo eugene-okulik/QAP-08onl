daffd486d1f34055d507e006fb359d3839f5311f
tmolofei <tmolofei@yandex.by>
2022-06-07 00:19:32 +0300

    Added changes in constant name in Homework_4

---
+++ homework/tatiana_molofei/Homework_5/hw_05_02.py
@@ -0,0 +1,11 @@
+# Напишите программу. Есть 2 переменные salary и bonus.
+# Salary - integer, bonus - boolean.
+# Если bonus - true, salary должна быть умножена на 10. Если false - нет
+
+salary = 168999
+bonus = False
+
+if bonus:
+    print(salary * 10)
+else:
+    print(salary)
