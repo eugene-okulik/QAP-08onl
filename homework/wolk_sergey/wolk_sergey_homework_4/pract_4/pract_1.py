e9ef2742234cf3b48dc26bc539fdce441cdefa02
Sergey1991wolk <svwolk1991@gmail.com>
2022-06-07 12:15:24 +0300

    workhome

--- homework/wolk_sergey/wolk_sergey_homework_4/pract_4/pract_1.py
+++
@@ -1,5 +0,0 @@
-# Заменить символ “#” на символ “/” в строке 'www.my_site.com#about'
-print('Начальная строка ' 'www.my_site.com#about')
-STR = 'www.my_site.com#about'
-STR2 = STR.replace("#", "/")
-print(STR2)
--- homework/wolk_sergey/wolk_sergey_homework_4/pract_4/pract_4.py
+++
@@ -1,7 +0,0 @@
-# Есть переменная с фамилией и именем: “Ivanov Ivan”. /
-# Распечатайте ее так, чтобы сначала шло имя, а затем фамилия. Т.е. “Ivan Ivanov”
-
-STR = ['Ivanov Ivan']
-REV = list(reversed(STR))
-LIST = ' '.join(REV)
-print(LIST)
