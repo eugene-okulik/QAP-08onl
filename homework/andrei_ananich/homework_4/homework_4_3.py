dda5de6223022c470a22ae7d1ce90fb7bbaa077b
andrei.ananich <andreiananich@yandex.by>
2022-06-08 20:53:19 +0300

    нашел середину строки и достал срез

--- homework/andrei_ananich/homework_4/homework_4_3.py
+++ homework/andrei_ananich/homework_4/homework_4_3.py
@@ -9,6 +9,7 @@
 A = 'Mauris fringilla odio sit amet pretium ultricies.\
 Pellentesque habitant morbi tristique'
 print(A[0:8])
-print(len(A)/2)
-print(A[43:47])
+
+LEN_STRING = int(len(A) / 2)
+print(A[LEN_STRING:LEN_STRING+4])
 print(A[3::3])
