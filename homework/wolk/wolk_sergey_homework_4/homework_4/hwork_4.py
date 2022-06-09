9c7d0a97a5c11e0495a0fe8c8d1e56223306c626
Sergey1991wolk <svwolk1991@gmail.com>
2022-06-09 10:32:03 +0300

    workhome

--- homework/wolk/wolk_sergey_homework_4/homework_4/hwork_4.py
+++ homework/wolk/wolk_sergey_homework_4/homework_4/hwork_4.py
@@ -5,4 +5,6 @@ USNUM = int(input('Попробуйте угадать загаданную ци
 while USNUM < NUM or USNUM > NUM:
     print("Попробуйте снова")
     USNUM = int(input('Попробуйте угадать загаданную цифру. Введите любую цифру '))
+while USNUM == NUM:
     print("Поздравляю! Вы угадали!")
+    break
--- homework/wolk/wolk_sergey_homework_4/pract_4/pract_3.py
+++ homework/wolk/wolk_sergey_homework_4/pract_4/pract_3.py
@@ -1,5 +1,7 @@
 # Создайте текстовую переменную “my name is name”.
 # Напечатайте ее, но вместо 2ого “name” вставьте ваше имя.
 NAME = 'my name is name'
-MY_NAME = NAME.replace("name", "sergey", 1)
+STR = NAME.split(" ")
+STR[3] = 'Sergey'
+MY_NAME = " ".join(STR)
 print(MY_NAME)
