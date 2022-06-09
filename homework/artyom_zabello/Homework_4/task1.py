77b9466c085482477a2c5988dc1a0b45f955ba76
Artyom Zabello <a.zabela@andersenlab.com>
2022-06-09 17:59:04 +0300

    Some fixes : homework1 , task1

--- homework/artyom_zabello/Homework_4/task1.py
+++ homework/artyom_zabello/Homework_4/task1.py
@@ -1,15 +1,8 @@
 TEXT = ('Etiam tincidunt neque erat, quis molestie enim'
-        'imperdiet vel. Integer urna nisl, facilisis vitae semper at,'
-        'dignissim vitae libero')
-
-
-def add_ing(text):
-    text = text.replace(',', "").replace(".", "").split(" ")
-    lst = []
-    for i in text:
-        lst.append(i+"ing")
-    return print(str(lst).replace("[", "").replace("]", "").
-                 replace("'", "").replace(",", ""))
-
-
-add_ing(TEXT)
+        'imperdiet vel. Integer urna nisl, facilisis vitae semper at, '
+        'dignissim vitae libero').split()
+LST = []
+for i in TEXT:
+    LST.append(i+"ing")
+LST = " ".join(LST).replace(",ing", "ing,").replace(".ing", "ing.")
+print(LST)
