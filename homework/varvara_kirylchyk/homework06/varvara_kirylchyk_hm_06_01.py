a6aedeba9de9739f49a39e760ec63dbac5ed7337
Varvara <kirylchyk@gmail.com>
2022-06-11 15:41:02 -0400

    varvara_kirylchyk_homework_06_done

---
+++ homework/varvara_kirylchyk/homework06/varvara_kirylchyk_hm_06_01.py
@@ -0,0 +1,31 @@
+# Напишите функцию на вход которой подается строка
+# Например, "cccbba" результат работы программы - строка “c3b2a"
+
+str1 = input("Input: ")
+
+def count_letters(line):
+
+    line += 'n'
+    list01 = []
+    str_length = len(line)
+    res_string = ""
+
+    counter = 1
+
+    for i in range(0, str_length-1):
+        if line[i] == line[i+1]:
+            counter += 1
+        else:
+            list01.append({line[i]: counter})
+            counter = 1
+    #print(list01)
+
+    for element in list01:
+        for key, value in element.items():
+            res_string += key
+            if value > 1:
+                res_string += str(value)
+
+    return res_string
+
+print(count_letters(str1))
