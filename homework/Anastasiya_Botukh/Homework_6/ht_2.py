522604cbd64eb5bf0abc0a2a45ffeb66a4c1bca5
Anastasiya <aisatsana@MacBook-Air-AISATSANA.local>
2022-06-11 15:18:57 +0300

    changes

--- homework/Anastasiya_Botukh/Homework_6/ht_2.py
+++ homework/Anastasiya_Botukh/Homework_6/ht_2.py
@@ -1,6 +1,6 @@
-actions = '1.plus. 2.minus. 3.multiply. 4.divide.'
-print(*actions.split(), sep='\n')
-actions = int(input('select the number that indicates the selected action:'))
+ACTIONS = '1.plus. 2.minus. 3.multiply. 4.divide.'
+print(*ACTIONS.split(), sep='\n')
+ACTIONS = int(input('select the number that indicates the selected action:'))
 NUM_1 = int(input('please, enter the first number:'))
 NUM_2 = int(input('please, enter the second number:'))
 
@@ -17,7 +17,7 @@ def user_actions(action, num_1, num_2):
             return 'not allowed'
         return num_1 / num_2
     else:
-        print({NUM_1 // NUM_2}, {NUM_1 % NUM_2})
+        return num_1 // num_2, num_1 % num_2
 
 
-print(user_actions(actions, NUM_1, NUM_2))
+print(user_actions(ACTIONS, NUM_1, NUM_2))
