386e8cd04222b28c07b312c1495ebc77247a4988
Anastasiya <aisatsana@MacBook-Air-AISATSANA.local>
2022-06-11 15:04:59 +0300

    homework_6

---
+++ homework/Anastasiya_Botukh/Homework_6/ht_1.py
@@ -0,0 +1,25 @@
+STRING = input('enter different letters:')
+
+
+def letter_count(user_str):
+    count_let = 1
+    result = ''
+    for key, value in enumerate(user_str):
+        if key == len(user_str) - 1:
+            if count_let == 1:
+                result += user_str[key]
+            else:
+                result += user_str[key] + str(count_let)
+            break
+        if user_str[key] == user_str[key + 1]:
+            count_let += 1
+        else:
+            if count_let == 1:
+                result += user_str[key]
+            else:
+                result += user_str[key] + str(count_let)
+            count_let = 1
+    return result
+
+
+print(letter_count(STRING))
---
+++ homework/Anastasiya_Botukh/Homework_6/ht_2.py
@@ -0,0 +1,23 @@
+actions = '1.plus. 2.minus. 3.multiply. 4.divide.'
+print(*actions.split(), sep='\n')
+actions = int(input('select the number that indicates the selected action:'))
+NUM_1 = int(input('please, enter the first number:'))
+NUM_2 = int(input('please, enter the second number:'))
+
+
+def user_actions(action, num_1, num_2):
+    if action == 1:
+        return num_1 + num_2
+    if action == 2:
+        return num_1 - num_2
+    if action == 3:
+        return num_1 * num_2
+    if action == 4:
+        if NUM_2 == 0:
+            return 'not allowed'
+        return num_1 / num_2
+    else:
+        print({NUM_1 // NUM_2}, {NUM_1 % NUM_2})
+
+
+print(user_actions(actions, NUM_1, NUM_2))
