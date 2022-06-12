f6fa231a3fd4379650c2a56fffc2dc3fe21dbbc9
Margarita Sokol <margarita.golovnina@yandex.by>
2022-06-12 14:10:51 +0300

    fixed formatting of comments in task1 deleted True or False from conditional statements in task2

--- homework/margarita_sokol/Homework_5/task2.py
+++ homework/margarita_sokol/Homework_5/task2.py
@@ -1,9 +1,11 @@
-# Напишите программу. Есть 2 переменные salary и bonus. Salary - integer, bonus - boolean.
-# Если bonus - true, salary должна быть умножена на 10. Если false - нет.
+# Напишите программу. Есть 2 переменные salary и bonus.
+# Salary - integer, bonus - boolean.
+# Если bonus - true, salary должна быть умножена на 10.
+# Если false - нет.
 
 SALARY = 10000
 BONUS = False
-if BONUS == True:
+if BONUS:
     print('$' + str(SALARY * 10))
-elif BONUS == False:
+else:
     print('$' + str(SALARY))
