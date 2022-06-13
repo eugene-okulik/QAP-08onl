dd75c498829cc640468322df7317581e52a93257
Sergey1991wolk <svwolk1991@gmail.com>
2022-06-13 14:55:29 +0300

    homework_5

---
+++ homework/wolk/wolk_sergey_homework_5/hwork_3.py
@@ -0,0 +1,13 @@
+
+A = "fizbbbbbuz"
+B = "b"
+C = "f"
+D = "z"
+E = "u"
+F = "i"
+
+print('fizbbbbbuz', 'b', '==', A.count(B))
+print('fizbbbbbuz', 'f', '==', A.count(C))
+print('fizbbbbbuz', 'z', '==', A.count(D))
+print('fizbbbbbuz', 'u', '==', A.count(E))
+print('fizbbbbbuz', 'i', '==', A.count(F))
