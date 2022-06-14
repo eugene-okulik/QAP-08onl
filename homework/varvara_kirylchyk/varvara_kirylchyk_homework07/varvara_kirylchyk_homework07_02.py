bf07e6fc90a4e6b5db913662a22b35c750cb5604
Varvara <kirylchyk@gmail.com>
2022-06-14 16:23:52 -0400

    varvara_kirylchyk_homework_07_done

---
+++ homework/varvara_kirylchyk/varvara_kirylchyk_homework07/varvara_kirylchyk_homework07_02.py
@@ -0,0 +1,23 @@
+#У вас интернет магазин, надо написать функцию которая проверяет
+# что введен правильный купон и он еще действителен
+
+from datetime import datetime
+
+DATE = str(input('Date: '))
+try:
+    check = datetime.strptime(DATE, "%B %d, %Y")
+except ValueError:
+    print("Incorrect format")
+
+code = input("Code: ")
+CORRECT_CODE = "123"
+
+now = datetime.now()
+CORRECT_DATE = now.strftime("%B %d, %Y")
+# print(CORRECT_DATE)
+
+def check_coupon(code_x, date_y):
+    if code_x == CORRECT_CODE and date_y >= CORRECT_DATE:
+        print("True")
+
+print(check_coupon(code, DATE))
