49c9137e069fe0ebfbf255f0270c9ec32e019a2f
tmolofei <tmolofei@yandex.by>
2022-06-15 19:37:07 +0300

    Added changes in code in task 1 from Homework_6

--- homework/tatiana_molofei/Homework_6/hw_06_01.py
+++ homework/tatiana_molofei/Homework_6/hw_06_01.py
@@ -1,6 +1,5 @@
 # Напишите функцию на вход которой подается строка, например, "cccbba"
 # результат работы программы - строка “c3b2a"
-#
 # Примеры для проверки работоспособности:
 # "cccbba" == "c3b2a"
 # "abeehhhhhccced" == "abe2h5c3ed"
@@ -8,17 +7,31 @@
 # "abcde" == "abcde"
 # "aaabbdefffff" == "a3b2def5"
 
-STRING = input()
-NEW_DICT = {}
+user_string = f'{input()} '
 
-for i in STRING:
-    if i not in NEW_DICT:
-        NEW_DICT[i] = 1
-    else:
-        NEW_DICT[i] += 1
 
-NEW_STRING = []
-for key, item in NEW_DICT.items():
-    NEW_STRING.append(f'{key}{item}')  # NEW_STRING.append("{}{}".format(key.capitalize(), item))
-RESULT = "".join(NEW_STRING)
+def count_letters(user_string):
+
+    count_index = 1
+    new_list = []
+    new_string = ''
+
+    for index in range(0, len(user_string) - 1):
+        if user_string[index] == user_string[index + 1]:
+            count_index += 1
+        else:
+            new_list.append({user_string[index]: count_index})
+            count_index = 1
+
+    for element in new_list:
+        for key, value in element.items():
+            new_string += key
+            if value > 1:
+                new_string += str(value)
+
+    return new_string
+
+
+RESULT = count_letters(user_string)
+
 print(RESULT)
