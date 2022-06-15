e8b8e0c0734b64f9111778dd65d1c330907ff223
Anastasia Machkova <nastya.machkova98@gmail.com>
2022-06-15 14:55:57 +0300

    task 1 and task 2 fixed secont time

--- homework/Anastasia_Machkova/Homework_6/task_1.py
+++ homework/Anastasia_Machkova/Homework_6/task_1.py
@@ -1,15 +1,15 @@
-def enum(S):
-    A = list(S)
-    C = []
-    for let in A:
-        if not let in C:
-            C.append(let)
+def enum(s_s):
+    a_a = list(s_s)
+    c_c = []
+    for let in a_a:
+        if let not in c_c:
+            c_c.append(let)
     result = ""
-    for let in C:
-        if S.count(let) == 1:
+    for let in c_c:
+        if s_s.count(let) == 1:
             result += let
         else:
-            result += let + str(S.count(let))
+            result += let + str(s_s.count(let))
     return result
 
 
--- homework/Anastasia_Machkova/Homework_6/task_2.py
+++ homework/Anastasia_Machkova/Homework_6/task_2.py
@@ -1,27 +1,27 @@
-def calculator(choice, A, B):
+def calculator(choice, a_a, b_b):
     if choice == 1:
-        result = A+B
+        result = a_a+b_b
     elif choice == 2:
-        result = A-B
+        result = a_a-b_b
     elif choice == 3:
-        result = A*B
+        result = a_a*b_b
     elif choice == 4:
-        if B == 0:
+        if b_b == 0:
             result = "Division by 0"
         else:
-            result = "Quotient" + str(int(A / B)) + "Remains" + str(int(A % B))
+            result = "Quotient " + str(int(a_a / b_b)) + "Remains " + str(int(a_a % b_b))
     else:
         result = "Not correct"
     return result
 
 
-OP = int(input("""Choose operation:
+op_r = int(input("""Choose operation:
 1. Addition
 2. Subtraction
 3. Multiplication
 4. Division\n"""))
 
-FIRST = float(input("Type first number:\n"))
-SECOND = float(input("Type second number:\n"))
+first_num = float(input("Type first number:\n"))
+second_num = float(input("Type second number:\n"))
 
-print(calculator(OP, FIRST, SECOND))
+print(calculator(op_r, first_num, second_num))
