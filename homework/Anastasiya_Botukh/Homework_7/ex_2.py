b5e1baf47e5cac0290e303ef2edeb440c956dc4f
Anastasiya <aisatsana@MacBook-Air-AISATSANA.local>
2022-06-16 20:36:32 +0300

    RETURN

--- homework/Anastasiya_Botukh/Homework_7/ex_2.py
+++ homework/Anastasiya_Botukh/Homework_7/ex_2.py
@@ -7,11 +7,10 @@ def check_coupon(entered_code, expiration_date):
     expiration_date = now.strptime(expiration_date, '%B %d, %Y')
     if entered_code is not CORRECT_CODE:
         return 'Your coupon is invalid.'
-    elif expiration_date >= now:
+    if expiration_date >= now:
         return 'Your coupon is valid.'
-    else:
-        return 'Your coupon is invalid.'
+    return 'Your coupon is invalid.'
 
 
-print(check_coupon("123", "July 16, 2022"))
+print(check_coupon("123", "July 1, 2022"))
 print(check_coupon("123", "June 2, 2022"))
