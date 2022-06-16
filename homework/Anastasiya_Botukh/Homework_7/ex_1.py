b664c7d1a780fc9f803e407b1eb81bb4a9aa7ab8
Anastasiya <aisatsana@MacBook-Air-AISATSANA.local>
2022-06-16 18:20:26 +0300

    exercises

---
+++ homework/Anastasiya_Botukh/Homework_7/ex_1.py
@@ -0,0 +1,32 @@
+STRING_ENCODE = str(input('Please, enter the words to encode:'))
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+
+
+def encode_string(string, shift):
+    encode = ''
+    for value in string:
+        index = ALPHABET.find(value)
+        if index > int(25 - int(shift)):
+            index = shift - (len(ALPHABET) - 1 - index) - 1
+        if index == -1:
+            encode += value
+        else:
+            encode += ALPHABET[index + int(shift)]
+    return encode
+
+
+def decode_string(string, shift):
+    decode = ''
+    for value in string:
+        index = ALPHABET.find(value)
+        if index == -1:
+            decode += value
+        if index < int(shift):
+            len(ALPHABET) - 1 - (shift - index)
+        else:
+            decode += ALPHABET[index - int(shift)]
+    return decode
+
+
+RESULT = encode_string(STRING_ENCODE, 3)
+print(encode_string(STRING_ENCODE, 3))
---
+++ homework/Anastasiya_Botukh/Homework_7/ex_2.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+CORRECT_CODE = '123'
+
+
+def check_coupon(entered_code, expiration_date):
+    now = datetime.now()
+    expiration_date = now.strptime(expiration_date, '%B %d, %Y')
+    if entered_code is not CORRECT_CODE:
+        return 'Your coupon is invalid.'
+    elif expiration_date >= now:
+        return 'Your coupon is valid.'
+    elif expiration_date < now:
+        return 'Your coupon is invalid.'
+
+
+print(check_coupon("123", "July 16, 2022"))
+print(check_coupon("123", "June 2, 2022"))
