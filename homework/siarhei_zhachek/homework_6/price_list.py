27923524dfea7b41d3bd0bd048010c46538ef5aa
SiarheiZhachek <SiarheiZhachek@yandex.by>
2022-06-16 22:26:31 +0300

    s_zhachek_homework_6_complete_fixed

--- homework/siarhei_zhachek/homework_6/price_list.py
+++ homework/siarhei_zhachek/homework_6/price_list.py
@@ -5,9 +5,8 @@ PRICE_LIST = '''тетрадь 50р
 альбом 120р
 пенал 300р
 рюкзак 500р'''
-MY_STR = PRICE_LIST.replace('0р', '0')
-MY_LIST = MY_STR.split('\n')
-NEW_LIST = []
-for positions in MY_LIST:
-    NEW_LIST.append(positions.split())
-print({key: value for key, value in NEW_LIST})
+
+MY_DICT = dict((position, int(price.strip('р')))
+               for position, price in (element.split()
+                                       for element in PRICE_LIST.split('\n')))
+print(MY_DICT)
