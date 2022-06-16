3c8768e90e0a3cfae1a3ad95cdf3c13abd7772f3
Anastasiya <aisatsana@MacBook-Air-AISATSANA.local>
2022-06-16 21:06:55 +0300

    generator

--- homework/Anastasiya_Botukh/Homework_6/ht_3.py
+++ homework/Anastasiya_Botukh/Homework_6/ht_3.py
@@ -5,10 +5,7 @@ PRICE_LIST = '''тетрадь 50р
 альбом 120р
 пенал 300р
 рюкзак 500р'''
-DICT = {}
 DICT_1 = PRICE_LIST.split()
 EVEN = [i for i in DICT_1 if not DICT_1.index(i) % 2]
 ODD = [i for i in DICT_1 if DICT_1.index(i) % 2]
-for value1, value2 in zip(EVEN, ODD):
-    DICT[value1] = int(value2.replace('р', ''))
-print(DICT)
+print({value1: int(value2.replace('р', '')) for value1, value2 in zip(EVEN, ODD)})
