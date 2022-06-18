806c1f33516ce0b71753bcd49789ca01ce9b8664
NikolayNazarov <nikolaynazarovvv216@gmail.com>
2022-06-18 23:52:17 +0300

    homework_7 done. Исправил ошибки

--- homework/nikolay_nazarov/Homework_7/task_1.py
+++ homework/nikolay_nazarov/Homework_7/task_1.py
@@ -2,46 +2,34 @@
 alfabet_eng = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
                'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-INPUT_STRING_FOR_ENCODING = "HEllo world!"
-INPUT_STRING_FOR_DECODING = "khoor zruog!"
+INPUT_STRING_FOR_ENCODING = "Hello world"
+INPUT_STRING_FOR_DECODING = "vszzc kcfzr"
 
 
-def decode_and_encode_dictionaries_generator(some_alfabet):
-    """
-    Функция составляет словарь для декодирования и кодирования текста.
-    """
+def decoder(some_text_to_decode, step):
     index_counter = 0
     counter_len = 0
     encoded_alfabet = []
-    while counter_len < len(some_alfabet):
+    while counter_len < len(alfabet_eng):
         try:
-            encoded_alfabet.append(some_alfabet[index_counter + 3])
+            encoded_alfabet.append(alfabet_eng[index_counter + step])
             index_counter += 1
             counter_len += 1
         except IndexError:
             index_counter = 0
-            while counter_len < len(some_alfabet):
-                encoded_alfabet.append(some_alfabet[index_counter])
+            while counter_len < len(alfabet_eng):
+                encoded_alfabet.append(alfabet_eng[index_counter])
                 index_counter += 1
                 counter_len += 1
-    encode_dictionary = dict(zip(some_alfabet, encoded_alfabet))
-    decode_dictionary = dict(zip(encoded_alfabet, some_alfabet))
-    return [decode_dictionary, encode_dictionary]
+    decode_dictionary = dict(zip(encoded_alfabet, alfabet_eng))
 
-
-def decoder(some_text_to_decode, decoder_dictionary):
-    """
-    Функция декодирует шифр цезаря, в качестве первого аргумента вводится
-    текст для кодирования или декодирования, в качестве второго - словарь полученный в результате
-    работы функции decode_and_encode_dictionaries_generator
-     """
     some_text_to_decode = some_text_to_decode.lower()
     some_text_decoded = ""
     decoder_counter = 0
     while decoder_counter < len(some_text_to_decode):
         if some_text_to_decode[decoder_counter] != " " \
                 and some_text_to_decode[decoder_counter] != "!":
-            some_text_decoded += (decoder_dictionary[some_text_to_decode[decoder_counter]])
+            some_text_decoded += (decode_dictionary[some_text_to_decode[decoder_counter]])
             decoder_counter += 1
         elif some_text_to_decode[decoder_counter] == " ":
             some_text_decoded += " "
@@ -53,13 +41,41 @@ def decoder(some_text_to_decode, decoder_dictionary):
             continue
     return some_text_decoded
 
+def encoder(some_text_to_encode, step):
+    index_counter = 0
+    counter_len = 0
+    encoded_alfabet = []
+    while counter_len < len(alfabet_eng):
+        try:
+            encoded_alfabet.append(alfabet_eng[index_counter + step])
+            index_counter += 1
+            counter_len += 1
+        except IndexError:
+            index_counter = 0
+            while counter_len < len(alfabet_eng):
+                encoded_alfabet.append(alfabet_eng[index_counter])
+                index_counter += 1
+                counter_len += 1
+    encode_dictionary = dict(zip(alfabet_eng, encoded_alfabet))
 
-e = decode_and_encode_dictionaries_generator(alfabet_eng)[1]
-d = decode_and_encode_dictionaries_generator(alfabet_eng)[0]
-# Для того чтобы закодировать текст, необходимо
-# в качестве первого аргумента ввести строку для кодирования (INPUT_STRING_FOR_ENCODING),
-# а в качестве второго переменную "d". """
-# Для того чтобы декодировать текст, необходимо
-# в качестве первого аргумента ввести строку для декодирования (INPUT_STRING_FOR_DECODING),
-# а в качестве второго переменную "e". """
-print(decoder(INPUT_STRING_FOR_DECODING, d))
+    some_text_to_encode = some_text_to_encode.lower()
+    some_text_encoded = ""
+    decoder_counter = 0
+    while decoder_counter < len(some_text_to_encode):
+        if some_text_to_encode[decoder_counter] != " " \
+                and some_text_to_encode[decoder_counter] != "!":
+            some_text_encoded += (encode_dictionary[some_text_to_encode[decoder_counter]])
+            decoder_counter += 1
+        elif some_text_to_encode[decoder_counter] == " ":
+            some_text_encoded += " "
+            decoder_counter += 1
+            continue
+        elif some_text_to_encode[decoder_counter] == "!":
+            some_text_encoded += "!"
+            decoder_counter += 1
+            continue
+    return some_text_encoded
+
+
+print(decoder(INPUT_STRING_FOR_DECODING, 14))
+print(encoder(INPUT_STRING_FOR_ENCODING, 14))
