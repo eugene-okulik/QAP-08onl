c450a31c7cfed161076c239d3b7a0a387e080b32
andrei.ananich <andreiananich@yandex.by>
2022-06-18 17:00:56 +0300

    changed function and getting correct total result

--- homework/andrei_ananich/homework_6/homework_6_1.py
+++ homework/andrei_ananich/homework_6/homework_6_1.py
@@ -8,14 +8,40 @@
 # "abcde" == "abcde"
 # "aaabbdefffff" == "a3b2def5"
 
-CHECK_STRING = "abeehhhhhccced"
-count = {}
-for s in CHECK_STRING:
-    if s in count:
-        count[s] += 1
-    else:
-        count[s] = 1
+# count = {}
+# for s in CHECK_STRING:
+#     if s in count:
+#         count[s] += 1
+#     else:
+#         count[s] = 1
+#
+# for key in count:
+#     if count[key] >= 1:
+#         print(key, count[key])
 
-for key in count:
-    if count[key] >= 1:
-        print(key, count[key])
+# CHECK_STRING = "abeehhhhhccced"
+
+CHECK_STRING = "abeehhhhhccced"
+
+def letter_count(check_str):
+    count_let = 1
+    result = ''
+    for key, _ in enumerate(check_str):
+        if key == len(check_str) - 1:
+            if count_let == 1:
+                result += check_str[key]
+            else:
+                result += check_str[key] + str(count_let)
+            break
+        if check_str[key] == check_str[key + 1]:
+            count_let += 1
+        else:
+            if count_let == 1:
+                result += check_str[key]
+            else:
+                result += check_str[key] + str(count_let)
+            count_let = 1
+    return result
+
+
+print(letter_count(CHECK_STRING))
