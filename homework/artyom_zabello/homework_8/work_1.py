5c4534fe330c80bc72019b57bb7dae4b3728995d
Artyom Zabello <a.zabela@andersenlab.com>
2022-06-18 18:45:30 +0300

    Homework 8 is done

---
+++ homework/artyom_zabello/homework_8/work_1.py
@@ -0,0 +1,32 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+
+def decar(func):
+    def wrapper(first, second, operation):
+        if first < 0 or second < 0:
+            operation = "*"
+        if first == second:
+            operation = "+"
+        if first > second:
+            operation = "-"
+        if first < second:
+            operation = "/"
+        return func(first, second, operation)
+
+    return wrapper
+
+
+@decar
+def calc(first, second, operation):
+    if operation == "*":
+        print(first * second)
+    if operation == "+":
+        print(first + second)
+    if operation == "-":
+        print(first - second)
+    if operation == "/":
+        print(first / second)
+
+
+calc(num1, num2, "+")
