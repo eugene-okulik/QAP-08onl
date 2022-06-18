e9c49648b6426bdb614461c35afd7f4fd6973b55
NikolayNazarov <nikolaynazarovvv216@gmail.com>
2022-06-18 23:58:54 +0300

    homework_7 done. Исправил ошибки

--- homework/nikolay_nazarov/Homework_7/task_2.py
+++ homework/nikolay_nazarov/Homework_7/task_2.py
@@ -5,22 +5,11 @@ CORRECT_CODE = '123'
 
 
 def date_encoder(some_date):
-    """
-    Преобразует строку с датой в правильный формат даты
-    :param some_date: str
-    :return: datetime.datetime
-    """
     encoded_date = datetime.datetime.strptime(some_date, '%B %d, %Y')
     return encoded_date
 
 
 def check_coupon(entered_code, expiration_date):
-    """
-    Функция проверяет что введен правильный купон и он действителен
-    :param entered_code: str
-    :param expiration_date: str
-    :return: bool
-    """
     if date_encoder(expiration_date) > datetime.datetime.now() and entered_code == CORRECT_CODE:
         return True
     return False
