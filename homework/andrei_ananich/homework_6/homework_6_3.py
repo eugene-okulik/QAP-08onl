7338e55f739d7e08b526daf9b9812d6320de5b09
andrei.ananich <andreiananich@yandex.by>
2022-06-19 12:01:17 +0300

    changed to generators

--- homework/andrei_ananich/homework_6/homework_6_3.py
+++ homework/andrei_ananich/homework_6/homework_6_3.py
@@ -20,11 +20,17 @@ PRICE_LIST = '''тетрадь 50р
 пенал 300р
 рюкзак 500р'''
 
-LIST = PRICE_LIST.split("\n")
-new_dict = {}
+DICT_1 = PRICE_LIST.split()
+EVEN = [i for i in DICT_1 if not DICT_1.index(i) % 2]
+ODD = [i for i in DICT_1 if DICT_1.index(i) % 2]
+print({value1: int(value2.replace('р', '')) for value1, value2 in zip(EVEN, ODD)})
 
-for i in LIST:
-    item = i.split()
-    new_dict.update({item[0]: int(item[1][:-1])})
-
-print(new_dict)
+# previous variant also works
+# LIST = PRICE_LIST.split("\n")
+# new_dict = {}
+#
+# for i in LIST:
+#     item = i.split()
+#     new_dict.update({item[0]: int(item[1][:-1])})
+#
+# print(new_dict)
