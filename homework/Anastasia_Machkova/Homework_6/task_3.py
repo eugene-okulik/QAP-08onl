ff94c0eab670ef0f94a8d43cc8f7b7b9f8e0da33
Anastasia Machkova <nastya.machkova98@gmail.com>
2022-06-20 11:27:01 +0300

    task 3 fixed

--- homework/Anastasia_Machkova/Homework_6/task_3.py
+++ homework/Anastasia_Machkova/Homework_6/task_3.py
@@ -7,5 +7,5 @@ PRICE_LIST = '''тетрадь 50р
 рюкзак 500р'''
 
 ELEMENTS = PRICE_LIST.split("\n")
-PAIRS = [i.split(" ") for i in ELEMENTS]
+PAIRS = [i.split() for i in ELEMENTS]
 print({k: int(v[:-1]) for k, v in PAIRS})
