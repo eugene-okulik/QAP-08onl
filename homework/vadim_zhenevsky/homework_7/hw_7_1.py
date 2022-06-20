e3081abffe35655981708a0554ed492db8656737
Vadim Zhenevsky <zhenevskyvadim@gmail.com>
2022-06-20 09:57:01 +0300

    FIXED PYLIN

--- homework/vadim_zhenevsky/homework_7/hw_7_1.py
+++ homework/vadim_zhenevsky/homework_7/hw_7_1.py
@@ -5,13 +5,13 @@ ALPHABET = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
 
 def encode(text_1):
     result_1 = ''
-    for x in text_1:
-        if x in ALPHABET:
-            position_now = ALPHABET.find(x)
+    for letter_1 in text_1:
+        if letter_1 in ALPHABET:
+            position_now = ALPHABET.find(letter_1)
             new_position = position_now + shift
             result_1 += ALPHABET[new_position]
         else:
-            result_1 += x
+            result_1 += letter_1
     return result_1
 
 
@@ -24,13 +24,13 @@ shift_2 = int(input('Enter shift length: '))
 
 def decode(text_2):
     result_2 = ''
-    for y in text_2:
-        if y in ALPHABET:
-            position_now = ALPHABET.find(y)
+    for letter_2 in text_2:
+        if letter_2 in ALPHABET:
+            position_now = ALPHABET.find(letter_2)
             new_position = position_now - shift_2
             result_2 += ALPHABET[new_position]
         else:
-            result_2 += y
+            result_2 += letter_2
     return result_2
 
 
