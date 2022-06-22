7cbaf8bac17fc8e9c5d6b4537024f52b467792cf
SiarheiZhachek <SiarheiZhachek@yandex.by>
2022-06-22 22:08:55 +0300

    s_zhachek_homework_7_coupon_fixed_calc

--- homework/siarhei_zhachek/homework_6/calculator.py
+++ homework/siarhei_zhachek/homework_6/calculator.py
@@ -1,10 +1,9 @@
 while True:
 
-
     def calculator():
         a_a = float(input('Enter the first value '))
         b_b = float(input('Enter the second value '))
-        c_c = input('Enter the operator ((+, -, /, *, dif) ')
+        c_c = input('Enter the operator ((+, -, /, *) ')
         if c_c == '+':
             result = a_a + b_b
         elif c_c == '-':
@@ -13,9 +12,7 @@ while True:
             result = a_a * b_b
         elif b_b != 0:
             if c_c == '/':
-                result = a_a / b_b
-            elif c_c == 'dif':
-                result = a_a // b_b
+                result = f'Quotient of division: {a_a // b_b}, Remains: {a_a % b_b}'
         elif b_b == 0:
             result = 'Division by zero is not possible!!!'
         return result
