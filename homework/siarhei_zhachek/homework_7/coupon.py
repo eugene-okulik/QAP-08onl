07e55e2610dc0e710d5301a5c422d8a4c55336b6
SiarheiZhachek <SiarheiZhachek@yandex.by>
2022-06-22 20:51:30 +0300

    s_zhachek_homework_7_coupon_fixed

--- homework/siarhei_zhachek/homework_7/coupon.py
+++ homework/siarhei_zhachek/homework_7/coupon.py
@@ -7,13 +7,13 @@ def check_coupon(entered_code, expiration_date):
     date = datetime.strptime(expiration_date, "%B %d, %Y")
     if entered_code == CORRECT_CODE:
         if now > date:
-            print('Your coupon has expired')
-            return False
-        else:
-            period = date - now
-            print(f'Your coupon is valid {period.days} days left')
-            return True
+            return 'Your coupon has expired'
+
+        period = date - now
+        return f'Your coupon is valid {period.days} days left'
+    return 'There is no such coupon'
 
 
 print(check_coupon("123", "July 9, 2022"))
+print()
 print(check_coupon("123", "June 2, 2022"))
