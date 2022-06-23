b0452305de349ea271bd4c45315887b3f3ac2ad6
NikolayNazarov <nikolaynazarovvv216@gmail.com>
2022-06-23 19:52:29 +0300

    homework_9 done.

--- homework/nikolay_nazarov/Homework_9/task_2.py
+++ homework/nikolay_nazarov/Homework_9/task_2.py
@@ -3,12 +3,24 @@ class Investments:
         self.inv_sum = inv_sum
         self.inv_time = inv_time
 
+    @staticmethod
+    def some_public_method_1():
+        pass
+
+    @staticmethod
+    def some_public_method_2():
+        pass
+
 
 class Bank:
     @staticmethod
     def deposit(class_object):
         profit = class_object.inv_sum * (1 + 0.1 / 12) ** (class_object.inv_time * 12)
         return profit
+
+    @staticmethod
+    def some_public_method_3():
+        pass
 
 
 inv1 = Investments(700000, 5)
