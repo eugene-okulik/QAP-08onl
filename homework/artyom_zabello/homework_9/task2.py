533d815bc873dba002a070782e86269afcf80d5c
Artyom Zabello <a.zabela@andersenlab.com>
2022-06-25 15:45:11 +0300

    Task 2 at homework 9 is done

---
+++ homework/artyom_zabello/homework_9/task2.py
@@ -0,0 +1,29 @@
+class Investment:
+    def __init__(self, amount, term):
+        self._amount = amount
+        self.term = float(term)
+
+    @property
+    def amount(self):
+        return self._amount
+
+    def invest(self):
+        profit = self._amount / 100 * (10/12)
+        return profit
+
+
+class Bank:
+
+    def public(self):
+        pass
+
+    @staticmethod
+    def deposit(dep):
+        """This method counts user's deposit according to investment.
+            Accepts investment as input """
+        result = dep.amount + (dep.invest()*(dep.term*12))
+        return print(result)
+
+
+inv1 = Investment(300, 1)
+Bank.deposit(inv1)
