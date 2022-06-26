08a42ce26f7af73f7695367e17b0c9e49f8fde76
NikolayNazarov <nikolaynazarovvv216@gmail.com>
2022-06-26 21:40:26 +0300

    homework_8 complete

--- homework/nikolay_nazarov/Homework_8/task_2.py
+++ homework/nikolay_nazarov/Homework_8/task_2.py
@@ -12,24 +12,13 @@ def num_list_generator(len_of_list=99):
     return [randint(0, 19) for i in range(len_of_list)]
 
 
-def dict_to_json_converter(some_dict):
-    dict_convertered_to_json = {}
-    i = 0
-    for key, value in some_dict.items():
-        dict_convertered_to_json[key] = {"word": value, "position": i}
-        i += 1
-    return dict_convertered_to_json
-
-
-def my_sort_func(some_list_to_sort, dict_for_sorting):
+def my_sort_func(some_list_to_sort):
     i = 0
     while i < len(some_list_to_sort):
         try:
-            if dict_for_sorting[some_list_to_sort[i]]["position"] \
-                    < dict_for_sorting[some_list_to_sort[i + 1]]["position"]:
+            if number_names[some_list_to_sort[i]] < number_names[some_list_to_sort[i+1]]:
                 i += 1
-            elif dict_for_sorting[some_list_to_sort[i]]["position"] \
-                    == dict_for_sorting[some_list_to_sort[i + 1]]["position"]:
+            elif number_names[some_list_to_sort[i]] == number_names[some_list_to_sort[i+1]]:
                 i += 1
             else:
                 temp = some_list_to_sort[i]
@@ -41,17 +30,6 @@ def my_sort_func(some_list_to_sort, dict_for_sorting):
     return some_list_to_sort
 
 
-# Сгенерируем рандомный список где какждое значение равно случайному значению от 0 до 19.
-# В функции укажем длинну списка (по умолчанию 99)
 Random_list = num_list_generator(10)
 print(Random_list)
-# Сгенерируем словарь на основе number_names ,
-# который будет иметь следующую структуру каждого элемента:
-# {0: {'word': 'zero', 'position': 0}
-# В качестве аргумента вводим словарь из условия задачи(number_names)
-Extended_dict = dict_to_json_converter(number_names)
-# И наконец, отсортируем рандомный список Random_list на основе дополненного словаря Extended_dict
-List_sorted = my_sort_func(Random_list, Extended_dict)
-# Выведем элементы полученного списка в одну строку черех пробел
-for elem in List_sorted:
-    print(elem, end=' ')
+print(my_sort_func(Random_list))
