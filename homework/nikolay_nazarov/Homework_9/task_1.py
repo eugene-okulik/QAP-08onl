4bed6076879b931c0e2acb68e6043aa8e8e67673
NikolayNazarov <nikolaynazarovvv216@gmail.com>
2022-06-26 18:35:03 +0300

    homework_9 done. В задании 1 добавил отдельный класс Library. В задании 2 в класс Investments добавил поле процент по вкладу deposit_percentage метод calculation_of_investment, в класс Bank переписал метод deposit, теперь он просто возвращает результат расчета доходности инвестиций calculation_of_investment

--- homework/nikolay_nazarov/Homework_9/task_1.py
+++ homework/nikolay_nazarov/Homework_9/task_1.py
@@ -5,57 +5,65 @@
 # (или которую уже кто-то читает - надо ему про это сказать).
 
 class Book:
-    _books_in_store = []
-    _who_took_book_dict = {}
-    _books_reserved = []
-    _who_reserve_book_dict = {}
-
     def __init__(self, book_title, author, pages, isbn):
         self.book_title = book_title
         self.author = author
         self.pages = pages
         self.isbn = isbn
-        Book._books_in_store.append(self)
+        Library.add_initialized_book(self)
+
+    def some_public_method_1(self):
+        pass
+
+    def some_public_method_2(self):
+        pass
+
+
+class Library:
+    _books_in_store = []
+    _who_took_book_dict = {}
+    _books_reserved = []
+    _who_reserve_book_dict = {}
 
     @staticmethod
     def print_who_took_book_dict():
         print("____________________________________")
         print("Какую книгу кто забрал")
-        print(Book._who_took_book_dict)
+        print(Library._who_took_book_dict)
         print("____________________________________")
 
     @staticmethod
     def print_who_reserve_book_dict():
         print("____________________________________")
         print("Какую книгу кто зарезервировал")
-        print(Book._who_reserve_book_dict)
+        print(Library._who_reserve_book_dict)
         print("____________________________________")
 
     @staticmethod
     def remove_book_from_storage(book, user):
-        Book._who_took_book_dict[book.book_title] = user.name
-        Book._books_in_store.remove(book)
+        Library._who_took_book_dict[book.book_title] = user.name
+        Library._books_in_store.remove(book)
 
     @staticmethod
     def add_book_to_storage(book):
-        Book._who_took_book_dict.pop(book.book_title)
-        Book._books_in_store.append(book)
+        Library._who_took_book_dict.pop(book.book_title)
+        Library._books_in_store.append(book)
 
     @staticmethod
     def add_book_to_reserved_list(book, user):
-        Book._who_reserve_book_dict[book.book_title] = user.name
-        Book._books_reserved.append(book)
+        Library._who_reserve_book_dict[book.book_title] = user.name
+        Library._books_reserved.append(book)
 
     @staticmethod
     def remove_book_from_reserved_list(book):
-        Book._who_reserve_book_dict.pop(book.book_title)
-        Book._books_reserved.remove(book)
+        Library._who_reserve_book_dict.pop(book.book_title)
+        Library._books_reserved.remove(book)
 
     @staticmethod
     def print_books_in_store():
         print("____________________________________")
         print("Список книг в наличии библиотеки")
-        for i in Book._books_in_store:
+        for i in Library._books_in_store:
             print(i.book_title)
         print("____________________________________")
 
@@ -63,21 +71,25 @@ class Book:
     def print_books_reserved():
         print("____________________________________")
         print("Список зарезервированных книг")
-        for i in Book._books_reserved:
+        for i in Library._books_reserved:
             print(i.book_title)
         print("____________________________________")
 
     @staticmethod
     def get_books_in_store():
-        return Book._books_in_store
+        return Library._books_in_store
 
     @staticmethod
     def get_who_took_book_dict():
-        return Book._who_took_book_dict
+        return Library._who_took_book_dict
 
     @staticmethod
     def get_who_reserve_book_dict():
-        return Book._who_reserve_book_dict
+        return Library._who_reserve_book_dict
+
+    @staticmethod
+    def add_initialized_book(book):
+        Library._books_in_store.append(book)
 
 
 class User:
@@ -86,32 +98,32 @@ class User:
         self.name = name
 
     def take_book(self, book):
-        if book in Book.get_books_in_store() and book not in Book.get_who_reserve_book_dict():
-            Book.remove_book_from_storage(book, self)
-        elif book not in Book.get_books_in_store():
+        if book in Library.get_books_in_store() and book not in Library.get_who_reserve_book_dict():
+            Library.remove_book_from_storage(book, self)
+        elif book not in Library.get_books_in_store():
             print(f"Книгу уже забрал пользователь с именем "
-                  f"{Book.get_who_took_book_dict()[book.book_title]}")
+                  f"{Library.get_who_took_book_dict()[book.book_title]}")
         else:
             print(f"Книгу уже зарезервировал пользователь с именем "
-                  f"{Book.get_who_reserve_book_dict()[book.book_title]}")
+                  f"{Library.get_who_reserve_book_dict()[book.book_title]}")
 
     def reserve_book(self, book):
-        if book not in Book.get_who_reserve_book_dict() and book in Book.get_books_in_store():
-            Book.add_book_to_reserved_list(book, self)
-        elif book in Book.get_who_reserve_book_dict():
+        if book not in Library.get_who_reserve_book_dict() and book in Library.get_books_in_store():
+            Library.add_book_to_reserved_list(book, self)
+        elif book in Library.get_who_reserve_book_dict():
             print(f"Книгу уже зарезервировал пользователь с именем "
-                  f"{Book.get_who_reserve_book_dict()[book.book_title]}")
+                  f"{Library.get_who_reserve_book_dict()[book.book_title]}")
         else:
             print(f"Книгу уже забрал пользователь с именем "
-                  f"{Book.get_who_took_book_dict()[book.book_title]}")
+                  f"{Library.get_who_took_book_dict()[book.book_title]}")
 
     @staticmethod
-    def unreserve_book(book):
-        Book.remove_book_from_reserved_list(book)
+    def cancel_reservation_of_book(book):
+        Library.remove_book_from_reserved_list(book)
 
     @staticmethod
     def return_book(book):
-        Book.add_book_to_storage(book)
+        Library.add_book_to_storage(book)
 
 
 user1 = User("Николай")
@@ -119,9 +131,9 @@ user2 = User("Павел")
 book1 = Book("Книга1", "Жюль Верн", 500, "10101010101")
 book2 = Book("Книга2", "Жюль Верн", 500, "10101010101")
 
-Book.print_who_reserve_book_dict()
+Library.print_who_reserve_book_dict()
 user1.take_book(book1)
-Book.print_who_reserve_book_dict()
+Library.print_who_reserve_book_dict()
 user2.reserve_book(book1)
-Book.print_who_took_book_dict()
-Book.print_who_reserve_book_dict()
+Library.print_who_took_book_dict()
+Library.print_who_reserve_book_dict()
--- homework/nikolay_nazarov/Homework_9/task_2.py
+++ homework/nikolay_nazarov/Homework_9/task_2.py
@@ -1,25 +1,23 @@
 class Investments:
-    def __init__(self, inv_sum, inv_time):
+    def __init__(self, inv_sum, inv_time, deposit_percentage=10):
         self.inv_sum = inv_sum
         self.inv_time = inv_time
+        self.deposit_percentage = deposit_percentage / 100
 
-    @staticmethod
-    def some_public_method_1():
-        pass
+    def calculation_of_investment(self):
+        profit = self.inv_sum * (1 + self.deposit_percentage / 12) ** (self.inv_time * 12)
+        return profit
 
-    @staticmethod
-    def some_public_method_2():
+    def some_public_method_1(self):
         pass
 
 
 class Bank:
     @staticmethod
-    def deposit(class_object):
-        profit = class_object.inv_sum * (1 + 0.1 / 12) ** (class_object.inv_time * 12)
-        return profit
+    def deposit(inv_object):
+        return inv_object.calculation_of_investment()
 
-    @staticmethod
-    def some_public_method_3():
+    def some_public_method_2(self):
         pass
 
 
