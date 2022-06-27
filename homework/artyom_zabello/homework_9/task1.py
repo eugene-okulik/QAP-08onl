4b57d17d57fb375a21d8db3891ac3fa687cee128
Artyom Zabello <a.zabela@andersenlab.com>
2022-06-28 01:08:56 +0300

    Fix task 1

--- homework/artyom_zabello/homework_9/task1.py
+++ homework/artyom_zabello/homework_9/task1.py
@@ -30,41 +30,46 @@ class Book:
     def flag_reserve(self):
         return self._reserved_flag
 
-    def booking(self, operation):
+    def booking(self, operation, user):
         """This is a function for booking. She receives an operation
-        code at the input(Y/N) and changes the flag depending on it"""
-        if operation == 'Y' and not self._reserved_flag:
+        code at the input(True/False) and changes the flag depending on it"""
+        if operation and not self._reserved_flag:
             setattr(self, '_reserved_flag', True)
-            return print(f"The book {self._name} was reserved")
-        if operation == 'N' and self._reserved_flag:
+            return print(f"The book {self._name} was reserved by {user}")
+        if not operation and self._reserved_flag:
             setattr(self, '_reserved_flag', False)
-            return print(f"The reservation was cancelled at book {self._name}")
+            return print(f"The reservation was cancelled at book {self._name} by {user}")
         raise ValueError("Operation is incorrect")
 
-    def get_book(self, operation):
+    def get_book(self, operation, user):
         """This is a function for get books at home. She receives an operation
-                code at the input(Y/N) and changes the flag depending on it"""
-        if operation == 'Y':
+                code at the input(True/False) and changes the flag depending on it"""
+        if operation:
             setattr(self, '_store_flag', False)
-            return print(f"You took the book {self._name}")
-        if operation == 'N':
+            return print(f"The user {user} took the book {self._name}")
+        if not operation:
             setattr(self, '_store_flag', True)
-            return print(f"You returned the book {self._name}")
+            return print(f"The user {user} returned the book {self._name}")
         raise ValueError("Operation is incorrect")
 
 
 class User:
-    def __init__(self, books, reserved_books):
-        self.books = books
-        self.reserved_books = reserved_books
+    def __init__(self, user_name):
+        self.books = {}
+        self.reserved_books = {}
+        self.user_name = user_name
+
+    @property
+    def get_user(self):
+        return self.user_name
 
     def book_a_book(self, book_name):
         """This function gives a possibility
         to book a book as a user.Gets an object of the book class as input"""
         if book_name.book not in self.books and bool(book_name.flag_store) and \
                 not bool(book_name.flag_reserve):
-            self.reserved_books.append(book_name.book)
-            book_name.booking('Y')
+            self.reserved_books = {book_name.book: self.user_name}
+            book_name.booking(True, self.user_name)
         else:
             print("The book is already reserved or located at user")
 
@@ -73,8 +78,8 @@ class User:
         to get book as a user.Gets an object of the book class as input"""
         if book_name.book not in self.books and bool(book_name.flag_store) and \
                 not bool(book_name.flag_reserve):
-            self.books.append(book_name.book)
-            book_name.get_book('Y')
+            self.books = {book_name.book: self.user_name}
+            book_name.get_book(True, self.user_name)
         else:
             print("The book is already reserved or located at user")
 
@@ -82,23 +87,22 @@ class User:
         """Cancellation of booking as a user.
         Gets an object of the book class as input"""
         if book_name.book in self.reserved_books:
-            self.reserved_books.remove(book_name.book)
-            book_name.booking('N')
+            self.reserved_books.clear()
+            book_name.booking(False, self.user_name)
 
     def return_book(self, book_name):
         """Returning of books as a user.
         Gets an object of the book class as input"""
         if book_name.book in self.books:
-            self.books.remove(book_name.book)
-            book_name.get_book('N')
+            self.books.clear()
+            book_name.get_book(False, self.user_name)
 
 
 book1 = Book('War and peace', 'Pushkin')
 book2 = Book('Fighting club', 'Palanik')
 
-user1 = User([], [])
-user2 = User([], [])
-
+user1 = User("Вася")
+user2 = User("Жорик")
 
 user1.book_a_book(book1)
 user2.read_a_book(book2)
