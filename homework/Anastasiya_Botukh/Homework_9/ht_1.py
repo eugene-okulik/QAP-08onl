0ff6dbaf4bcacd00f819123b9c64b0307f54638e
Anastasiya <aisatsana@MacBook-Air-AISATSANA.local>
2022-07-01 14:10:22 +0300

    homework_9

---
+++ homework/Anastasiya_Botukh/Homework_9/ht_1.py
@@ -0,0 +1,51 @@
+class Book:
+
+    def __init__(self, name, author, pages, isbn, reserved, taken):
+        self.name = name
+        self.author = author
+        self.pages = pages
+        self.isbn = isbn
+        self.reserved = reserved
+        self.taken = taken
+
+    def set_reserved(self, reserved):
+        self.reserved = reserved
+
+    def set_taken(self, taken):
+        self.taken = taken
+
+
+class User:
+
+    @staticmethod
+    def take(book):
+        if book.taken or book.reserved is True:
+            print("You can't take this book -", book.name)
+        else:
+            print("You can take this book -", book.name)
+            book.set_taken(True)
+
+    @staticmethod
+    def return_book(book):
+        book.set_taken(False)
+        print('Thank you for returning')
+
+    @staticmethod
+    def reserved_book(book):
+        book.set_reserved(True)
+        print('You reserved this book')
+
+
+book_1 = Book('Duma Key', '12345', 670, 'Stephen King', False, False)
+book_2 = Book('Kudjo', '67890', 400, 'Stephen King', False, False)
+book_3 = Book('Thinner', '102938', 309, 'Stephen King', False, False)
+
+user_1 = User()
+user_2 = User()
+user_3 = User()
+
+user_1.take(book_3)
+user_2.reserved_book(book_2)
+user_3.take(book_2)
+user_3.reserved_book(book_1)
+user_1.return_book(book_3)
---
+++ homework/Anastasiya_Botukh/Homework_9/ht_2.py
@@ -0,0 +1,22 @@
+class Investment:
+    def __init__(self, amount, term):
+        self.amount = amount
+        self.term = term
+
+
+class Bank:
+    @staticmethod
+    def deposit(invest):
+        monthly_percent = 10 / 12
+        months = invest.term * 12
+        total_amount = invest.amount
+        while months > 0:
+            total_amount = total_amount + (total_amount / 100 * monthly_percent)
+            months = months - 1
+            # print(total_amount)
+        return total_amount
+
+
+investment = Investment(2000, 5)
+bank = Bank()
+print(bank.deposit(investment))
