22ad7d1bbf6904c5ff888b69f068023444ea6ebf
NikolayNazarov <nikolaynazarovvv216@gmail.com>
2022-07-03 21:01:16 +0300

    homework_10 complete

--- homework/nikolay_nazarov/Homework_10/task_1.py
+++ homework/nikolay_nazarov/Homework_10/task_1.py
@@ -1,85 +1,117 @@
 class Flower:
-    def empty_public_method_1(self):
-        pass
+    def __init__(self, **kwargs):
+        self.name = kwargs.get("name")
+        self.lifetime = kwargs.get("lifetime")
+        self.color = kwargs.get("color")
+        self.length = kwargs.get("length")
+        self.price = kwargs.get("price")
 
-    def empty_public_method_2(self):
-        pass
+    def set_color(self, color):
+        self.color = color
+
+    def set_price(self, price):
+        self.price = price
 
 
 class Rose(Flower):
-    def __init__(self):
-        self.flower_name = "Rose"
-        self.color = "Red"
-        self.price = 500
-        self.lifetime_hours = 40
-        self.stem_length = 70
+    def __init__(self, **kwargs):
+        super().__init__(
+            name=kwargs.get("name"),
+            lifetime=kwargs.get("lifetime"),
+            color=kwargs.get("color"),
+            length=kwargs.get("length"),
+            price=kwargs.get("price")
+        )
 
 
 class Verbena(Flower):
-    def __init__(self):
-        self.flower_name = "Verbena"
-        self.color = "Blue"
-        self.price = 800
-        self.lifetime_hours = 20
-        self.stem_length = 30
+    def __init__(self, **kwargs):
+        super().__init__(
+            name=kwargs.get("name"),
+            lifetime=kwargs.get("lifetime"),
+            color=kwargs.get("color"),
+            length=kwargs.get("length"),
+            price=kwargs.get("price")
+        )
 
 
 class Peony(Flower):
-    def __init__(self):
-        self.flower_name = "Peony"
-        self.color = "Yellow"
-        self.price = 100
-        self.lifetime_hours = 10
-        self.stem_length = 50
+    def __init__(self, **kwargs):
+        super().__init__(
+            name=kwargs.get("name"),
+            lifetime=kwargs.get("lifetime"),
+            color=kwargs.get("color"),
+            length=kwargs.get("length"),
+            price=kwargs.get("price")
+        )
 
 
 class Bouquet:
-    def __init__(self, *flowers):
-        self.content = list(flowers)
+    def __init__(self):
+        self.components = []
+
+    def add_flower(self, flower_):
+        self.components.append(flower_)
+
+    def full_amount(self):
+        return len(self.components)
+
+    def full_price(self):
+        res = 0
+        for flower_ in self.components:
+            res = res + flower_.price
+        return res
 
     def lifetime_of_bouquet(self):
-        result = 0
-        for i in self.content:
-            result += i.lifetime_hours
-        result = round(result / len(self.content), 1)
-        return result
-
-    def sort_by_lifetime_hours(self):
-        i = 0
-        while i < len(self.content):
-            try:
-                if self.content[i].lifetime_hours < self.content[i + 1].lifetime_hours:
-                    i += 1
-                elif self.content[i].lifetime_hours == self.content[i + 1].lifetime_hours:
-                    i += 1
-                else:
-                    temp = self.content[i]
-                    self.content[i] = self.content[i + 1]
-                    self.content[i + 1] = temp
-                    i = 0
-            except IndexError:
-                break
-        return self.content
+        res = 0
+        for flower_ in self.components:
+            res = res + flower_.lifetime
+        return res // self.full_amount()
 
     def find_flower_by_color(self, color):
-        for i in self.content:
-            if i.color == color:
-                return i.flower_name
-        return "Нет цветка в таким цветом"
+        res = []
+        for flower_ in self.components:
+            if flower_.color == color:
+                res.append(flower_.name)
+                return res
+        return "Такого цвета нет"
 
-    def is_where_flower_with_price(self, price):
-        for i in self.content:
-            if i.price == price:
-                return i.flower_name
-        return "Нет цветка с такой стоимостью"
+    def find_price_of_flower(self, price):
+        res = []
+        for flower_ in self.components:
+            if flower_.price == price:
+                res.append(flower_.name)
+                return res
+        return "Нет цветка такой стоимости"
+
+    def find_flower_in_bouquet(self, name):
+        for flower_ in self.components:
+            if flower_.name == name:
+                return "Этот цветок есть в букете"
+            return "Нет такого цветка в букете"
+
+    def find_flower_by_length(self, length):
+        res = []
+        for flower_ in self.components:
+            if flower_.length == length:
+                res.append(flower_.name)
+                return res
+        return "Нет цветка c такой длинной стебля в букете"
 
 
-Rose1 = Rose()
-Verbena1 = Verbena()
-Peony1 = Peony()
-Bouquet1 = Bouquet(Rose1, Verbena1, Peony1)
-print(Bouquet1.lifetime_of_bouquet())
-print(Bouquet1.sort_by_lifetime_hours())
-print(Bouquet1.content)
-print(Bouquet1.find_flower_by_color("Red"))
-print(Bouquet1.is_where_flower_with_price(100))
+flower = Flower()
+bouquet = Bouquet()
+rose = Rose(name="Rose", lifetime=50, length=40, color="Blue", price=200)
+verbena = Verbena(name="Verbena", lifetime=25, length=50, color="Red", price=150)
+peony = Peony(name="Peony", lifetime=15, length=30, color="Orange", price=100)
+bouquet.add_flower(rose)
+bouquet.add_flower(verbena)
+bouquet.add_flower(peony)
+print(bouquet.components)
+print(bouquet.lifetime_of_bouquet())
+print(bouquet.find_flower_by_color("Red"))
+print(bouquet.find_price_of_flower(200))
+print(bouquet.find_flower_in_bouquet("Rose"))
+print(bouquet.full_price())
+print(bouquet.full_amount())
+print(bouquet.find_flower_by_length(30))
