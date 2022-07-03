260cfa1052a896566409a0484bc6e27801c726c0
tmolofei <tmolofei@yandex.by>
2022-07-04 02:37:54 +0300

    Added changes in task 2 from Homework_7

--- homework/tatiana_molofei/Homework_7/hw_07_02.py
+++ homework/tatiana_molofei/Homework_7/hw_07_02.py
@@ -3,21 +3,17 @@
 
 
 from datetime import datetime
+# from datetime import date
+
 
 correct_code = set(range(100, 201))
 
-expiration_date = datetime(2022, 7, 31)
-expiration_date = expiration_date.strftime("%B %d, %Y")
-
-curent_day = datetime.now()
-curent_day = curent_day.strftime("%B %d, %Y")
-
-# user_code = int(input(f'Enter your coupon number: '))
-# user_date = input(f'Enter your coupon expiration date Month name dd, yyyy: ')
+current_day = datetime.now()
 
 
 def check_coupon(user_code, user_date):
-    if expiration_date >= user_date >= curent_day and user_code in correct_code:
+    user_date_python = datetime.strptime(user_date, "%B %d, %Y")
+    if user_date_python >= current_day and user_code in correct_code:
         return True
     return False
 
