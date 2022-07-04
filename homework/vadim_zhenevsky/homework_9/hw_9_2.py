54589f1cd023b7586379e6b1836651b91bb9bc95
Vadim Zhenevsky <zhenevskyvadim@gmail.com>
2022-07-04 15:58:42 +0300

    homework 9 done second task

---
+++ homework/vadim_zhenevsky/homework_9/hw_9_2.py
@@ -0,0 +1,28 @@
+class Investments:
+    def __init__(self, amount, rate, time):
+        self._amount = amount
+        self.rate = rate
+        self._time = time
+
+    @property
+    def time(self):
+        return self._time
+
+    @property
+    def amount(self):
+        return self._amount
+
+
+class Bank:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def deposit(num):
+        simple_interest = (num.amount * num.time * num.rate) / 100
+        profit = num.amount + simple_interest
+        return profit
+
+
+Invest = Investments(5000, 15, 5)
+print(Bank.deposit(Invest))
