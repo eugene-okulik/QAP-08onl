c885fc44d1a8d580962a6d68ddca474a38a5640e
Vadim Zhenevsky <zhenevskyvadim@gmail.com>
2022-07-05 23:36:33 +0300

    homework_10 done

---
+++ homework/vadim_zhenevsky/homework_10/hw_10.py
@@ -0,0 +1,112 @@
+class Flowers:
+    def __init__(self, freshness, color, price, name):
+        self.freshness = freshness
+        self.color = color
+        self.price = price
+        self.name = name
+
+    def get_flowers(self):
+        return [self.freshness, self.color, self.price, self.name]
+
+    def set_freshness(self, freshness):
+        self.freshness = freshness
+
+    def set_color(self, color):
+        self.color = color
+
+    def set_price(self, price):
+        self.price = price
+
+    def set_name(self, name):
+        self.name = name
+
+
+class Tulips(Flowers):
+    def __init__(self, freshness=None, color=None, price=None, name=None):
+        super().__init__(freshness, color, price, name)
+
+
+class Roses(Flowers):
+    def __init__(self, freshness=None, color=None, price=None, name=None):
+        super().__init__(freshness, color, price, name)
+
+
+class Orchids(Flowers):
+    def __init__(self, freshness=None, color=None, price=None, name=None):
+        super().__init__(freshness, color, price, name)
+
+
+class Chamomiles(Flowers):
+    def __init__(self, freshness=None, color=None, price=None, name=None):
+        super().__init__(freshness, color, price, name)
+
+
+class Lily(Flowers):
+    def __init__(self, freshness=None, color=None, price=None, name=None):
+        super().__init__(freshness, color, price, name)
+
+
+class Bouquet:
+    def __init__(self, flowers):
+        self.flowers = flowers
+
+    def bouquet_freshness(self):
+        total_freshness = 0
+        for flower in self.flowers:
+            total_freshness = total_freshness + flower.freshness
+        return total_freshness / len(self.flowers)
+
+    def price_bouquet(self):
+        total_price = 0
+        for flower in self.flowers:
+            total_price = total_price + flower.price
+        return total_price
+
+    def find_color(self, color):
+        result = []
+        for flower in self.flowers:
+            if flower.color == color:
+                result.append(flower.name)
+        return result if len(result) > 0 else 'there is no such color'
+
+    def find_name(self, name):
+        result = []
+        for flower in self.flowers:
+            if flower.name == name:
+                result.append(flower.name)
+        return result if len(result) > 0 else 'there is no such flower'
+
+    def find_price(self, price):
+        result = []
+        for flower in self.flowers:
+            if flower.price == price:
+                result.append(flower.name)
+        return result if len(result) > 0 else 'there is no such price'
+
+    def find_freshness(self, freshness):
+        result = []
+        for flower in self.flowers:
+            if flower.freshness == freshness:
+                result.append(flower.name)
+        return result if len(result) > 0 else 'there is no such freshness'
+
+
+tulip = Tulips(6, 'yellow', 4, 'Tulip')
+rose = Roses(4, 'white', 9, 'Rose')
+orchid = Orchids(9, 'white', 14, 'Orchid')
+chamomile = Chamomiles(3, 'blue', 7, 'Chamomile')
+lily = Lily(12, 'purple', 6, 'Lily')
+flowers1 = [tulip, rose, orchid, chamomile, lily]
+flowers2 = [rose, orchid, lily]
+flowers3 = [tulip, chamomile]
+bouquet1 = Bouquet(flowers1)
+bouquet2 = Bouquet(flowers2)
+bouquet3 = Bouquet(flowers3)
+print(bouquet1.find_freshness(9))
+print(bouquet2.find_price(7))
+print(bouquet1.find_color("blue"))
+print(bouquet2.find_color('white'))
+print(bouquet3.find_freshness(7))
+print(bouquet1.price_bouquet())
+print(bouquet3.bouquet_freshness())
+print(bouquet2.price_bouquet())
