132d22e2dcc99e152d56350096c4916738c99c08
Anastasiya <aisatsana@MacBook-Air-AISATSANA.local>
2022-07-05 13:28:31 +0300

    methods

--- homework/Anastasiya_Botukh/Homework_9/ht_2.py
+++ homework/Anastasiya_Botukh/Homework_9/ht_2.py
@@ -1,7 +1,8 @@
 class Investment:
-    def __init__(self, amount, term):
+    def __init__(self, amount, term, profitability_calculation):
         self.amount = amount
         self.term = term
+        self.profitability_calculation = profitability_calculation
 
 
 class Bank:
@@ -10,16 +11,19 @@ class Bank:
 
     @staticmethod
     def deposit(invest):
-        monthly_percent = 10 / 12
+        monthly_profitability_calculation = 10 / 12
         months = invest.term * 12
         total_amount = invest.amount
         while months > 0:
-            total_amount = total_amount + (total_amount / 100 * monthly_percent)
+            total_amount = total_amount + (total_amount / 100 * monthly_profitability_calculation)
             months = months - 1
             # print(total_amount)
         return total_amount
 
+    def public_method(self):
+        pass
 
-investment = Investment(2000, 5)
+
+investment = Investment(2000, 5, 4)
 bank = Bank()
 print(bank.deposit(investment))
