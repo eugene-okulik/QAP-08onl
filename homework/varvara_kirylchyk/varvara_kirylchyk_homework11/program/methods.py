cb9b918c744ab520f84660117ade2678da892a5a
Varvara <kirylchyk@gmail.com>
2022-07-05 16:56:16 -0400

    updates

--- homework/varvara_kirylchyk/varvara_kirylchyk_homework11/program/methods.py
+++ homework/varvara_kirylchyk/varvara_kirylchyk_homework11/program/methods.py
@@ -1,7 +1,7 @@
-from package import Book
+from package.book import Book
 
 book1 = Book('New Tales', 2020, 150)
-book2 = Book('Old Tales', 1980 , 90)
+book2 = Book('Old Tales', 1980, 90)
 
 print(book1.is_expensive())
 print(book2.is_modern())
--- homework/varvara_kirylchyk/varvara_kirylchyk_homework11/varvara_kirylchyk_homework11.py
+++ homework/varvara_kirylchyk/varvara_kirylchyk_homework11/varvara_kirylchyk_homework11.py
@@ -9,15 +9,11 @@
 # Обработайте это исключение и подскажите пользователю в каком формате нужно вводить дату
 
 from datetime import datetime
-from datetime import date
 
 user_date = input('Enter date: ')
-python_date = datetime.strptime(user_date, '%d/%m/%Y')
 
-while True:
-    try:
-        if isinstance(python_date, date):
-            print("Date format is correct")
-            break
-    except ValueError:
-        print("Date type is incorrect. Please apply the format: DD/MM/YYYY")
+try:
+    python_date = datetime.strptime(user_date, '%d/%m/%Y')
+    print("The date format is correct.")
+except ValueError:
+    print("Date type is incorrect. Please apply the format: DD/MM/YYYY")
