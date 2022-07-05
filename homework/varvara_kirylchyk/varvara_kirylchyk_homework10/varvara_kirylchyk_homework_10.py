6d7df4dc499f63f53b3310b6d6fd3155a8a95c7a
Varvara <kirylchyk@gmail.com>
2022-07-04 22:10:58 -0400

    updates

--- homework/varvara_kirylchyk/varvara_kirylchyk_homework10/varvara_kirylchyk_homework_10.py
+++ homework/varvara_kirylchyk/varvara_kirylchyk_homework10/varvara_kirylchyk_homework_10.py
@@ -1,43 +1,72 @@
 # Цветочница
-#
-# Создать классы цветов: общий класс для всех цветов и классы для нескольких видов.
+# Создать классы цветов: общий класс для всех цветов
+# и классы для нескольких видов.
 # Создать экземпляры цветов.
-
 # Собрать букет (букет - еще один класс) (можно использовать аксессуары)
 # с определением его стоимости.
-
 # Для букета создать метод,
-# который определяет время его увядания по среднему времени жизни всех цветов в букете.
-#
+# который определяет время его увядания по среднему времени жизни
+# всех цветов в букете.
 # Позволить сортировку цветов в букете на основе различных параметров
 # (свежесть/цвет/длина стебля/стоимость)(это тоже методы)
-
 # Реализовать поиск цветов в букете по каким-нибудь параметрам
 # (например, по среднему времени жизни) (и это тоже метод).
-
 # Узнать, есть ли цветок определенного типа в букете.
+# max age of rose is 5; fresh rose when current age <=20% of max age
+# max age of peony is 10; fresh peony when current age <= 50% max age
 
-class Plant:
-    def __init__(self, name=None, age=None, color=None, price=None):
-        self.name = name
-        self.age = age
-        self.color = color
-        # self.length = length
-        self.price = price
+import abc
+from dataclasses import dataclass
 
-    def is_expensive(self):
-        return 'expensive' if self.price > 10 else 'cheap'
 
+@dataclass
+class Plant(abc.ABC):
+    name: str
+    max_age: int
+    current_age: int
+    color: str
+    length: int
+    price: float
+    petal_shape: str
+    # thorns: bool
+
+    # @abc.abstractmethod
+    # def is_expensive(self):
+    #     pass
+
+    @abc.abstractmethod
     def is_fresh(self):
-        return 'fresh' if self.age < 5 else 'not fresh'
+        pass
 
-class Flower(Plant):
-    def __init__(self, name, age=None, color=None, price=None):
-        super().__init__(name, age, color, price)
 
-class Flower2(Plant):
-    def __init__(self, name, age=None, color=None, price=None):
-        super().__init__(name, age, color, price)
+class Rose(Plant):
+    def is_fresh(self):
+        return self.current_age <= 0.2 * self.max_age
+
+    def dummy(self):
+        pass
+
+
+class Peony(Plant):
+    def is_fresh(self):
+        return self.current_age <= 0.5 * self.max_age
+
+    def dummy(self):
+        pass
+
+
+rose1 = Rose(name="Rose", max_age=5, current_age=1, color="red",
+             length=10, price=100, petal_shape="round")
+peony1 = Peony(name="Peony", max_age=10, current_age=8, color="white",
+               length=20, price=80, petal_shape="")
+print(rose1.is_fresh())
+print(peony1.is_fresh())
+
+rose2 = Rose(name="Rose", max_age=7, current_age=2, color="red",
+             length=10, price=100, petal_shape="round")
+peony2 = Peony(name="Peony", max_age=5, current_age=9, color="red",
+               length=10, price=100, petal_shape="round")
+
 
 class Bunch:
     def __init__(self):
@@ -47,10 +76,7 @@ class Bunch:
         self.flowers.append(flower)
 
     def total_amount(self):
-        result = 0
-        for _ in self.flowers:
-            result = result + 1
-        return result
+        return len(self.flowers)
 
     def total_price(self):
         result = 0
@@ -60,40 +86,27 @@ class Bunch:
 
     # Увядание по среднему времени жизни всех цветов в букете, в днях
     def average_age(self):
-        result = 0
-        for flower in self.flowers:
-            result = result + flower.age
-            average = result // self.total_amount()
-        return average
+        return sum([f.current_age for f in self.flowers]) / len(self.flowers)
 
-    # Сортировка по цвету
+    # выборка цветов по цвету
     def get_color(self, color):
-        result = []
-        for flower in self.flowers:
-            if flower.color == color:
-                result.append(flower.name)
-        return result
+        return [f for f in self.flowers if f.color == color]
 
     @staticmethod
     def is_expensive_stat(price):
-        return 'expensive' if price > 10 else 'cheap'
+        return price > 10
 
-Plant = Plant()
+
 Bunch = Bunch()
 
-Daisy = Flower("Daisy", 6, "pupple", 15)
-Camomile = Flower2("Camomile", 6, "white", 5)
-Rose = Flower("Rose", 5, "red", 5)
-
-Bunch.add_flower(Daisy)
-Bunch.add_flower(Camomile)
-Bunch.add_flower(Rose)
-# print(Bunch)
+Bunch.add_flower(rose1)
+Bunch.add_flower(rose2)
+Bunch.add_flower(peony1)
+Bunch.add_flower(peony2)
 
 print(Bunch.total_amount())
 print(Bunch.total_price())
 print(Bunch.average_age())
 print(Bunch.get_color("red"))
 print(Bunch.is_expensive_stat(15))
-print(Rose.is_expensive())
-print(Flower2)
+print(rose1)
