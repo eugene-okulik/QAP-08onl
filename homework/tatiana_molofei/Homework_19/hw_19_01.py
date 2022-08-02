d541b883bbee57d9f9baf5b5ee619fa240a6d9a8
TatiMolofei <ps_tanusha@mail.ru>
2022-08-02 20:36:53 +0300

    Homework_19 added

---
+++ homework/tatiana_molofei/Homework_19/hw_19_01.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+options = Options()
+options.add_argument('start-maximized')
+driver = webdriver.Chrome(options=options)
+
+driver.get('http://automationpractice.com/index.php')
+
+element = driver.find_element(By.ID, "contact-link")
+element.click()
+
+element = driver.find_element(By.ID, "uniform-id_contact")
+element.click()
+select = Select(driver.find_element(By.ID, "id_contact"))
+select.select_by_value("2")
+
+element = driver.find_element(By.ID, "email")
+element.click()
+element.send_keys('tmolofei@yandex.by')
+
+element = driver.find_element(By.ID, "id_order")
+element.click()
+element.send_keys(12358)
+
+element = driver.find_element(By.ID, "message")
+element.click()
+element.send_keys('Hello, Eugene')
+
+element = driver.find_element(By.ID, "submitMessage")
+element.click()
+
+# message = driver.find_element(By.NAME, "alert alert-success")
+# print(message)
