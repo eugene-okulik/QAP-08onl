94897d1e8246b93979a850bf97ffa3350c153b13
tatiana molofei <tmolofey@yandex.by>
2022-08-08 02:19:13 +0300

    Added task_2 from Homework_20

--- homework/tatiana_molofei/Homework_20/hw_20_02.py
+++ homework/tatiana_molofei/Homework_20/hw_20_02.py
@@ -9,65 +9,63 @@ options = Options()
 options.add_argument('start-maximized')
 chrome_driver = webdriver.Chrome(options=options)
 
-first_name = 'Tatiana'
-last_name = 'Molofei'
-email = 'tmolofei@yandex.by'
+FIRST_NAME = 'Tatiana'
+LAST_NAME = 'Molofei'
+EMAIL = 'tmolofei@yandex.by'
 male, female, other = 0, 1, 2
-mobile = '804458371700'
-year_of_birth = 1988
-month_of_birth = 12
-day_of_birth = 18
-subject = "English"
+MOBILE = '804458371700'
+YEAR_OF_BIRTH = 1988
+MONTH_OF_BIRTH = 12
+DAY_OF_BIRTH = 18
+SUBJECT = "English"
 sports, reading, music = 3, 4, 5
-current_address = 'Priluki, Mayskaya, 10'
+ADDRESS = 'Priluki, Mayskaya, 10'
 
 
 def filling_practice_form(driver):
     driver.get('https://demoqa.com/automation-practice-form')
 
     first_name_fill = driver.find_element(By.XPATH, '//input[@id="firstName"]')
-    first_name_fill.send_keys(first_name)
+    first_name_fill.send_keys(FIRST_NAME)
 
     last_name_fill = driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Last Name"]')
-    last_name_fill.send_keys(last_name)
+    last_name_fill.send_keys(LAST_NAME)
 
     email_fill = driver.find_element(By.ID, 'userEmail')
-    email_fill.send_keys(email)
+    email_fill.send_keys(EMAIL)
 
     gender_fill = driver.find_elements(By.XPATH, '//label[@class="custom-control-label"]')
     gender_fill[female].click()
 
-    # The below code is not working and I don't know why
-    # gender_fill = driver.find_element(By.XPATH, '//input[@class="custom-control-input"][@value="Female"]')
-    # gender_fill.click()
-
     mobile_fill = driver.find_elements(By.XPATH, '//input[@class=" mr-sm-2 form-control"]')
-    mobile_fill[2].send_keys(mobile)
-    if len(mobile) != 10 or mobile.isdigit() is False:
+    mobile_fill[2].send_keys(MOBILE)
+    if len(MOBILE) != 10 or MOBILE.isdigit() is False:
         print("Error. The phone number must be 10 digits.")
 
     date_of_birth_fill = driver.find_element(By.ID, 'dateOfBirthInput')
     date_of_birth_fill.click()
     month_fill = Select(driver.find_element(By.CLASS_NAME, 'react-datepicker__month-select'))
-    month_fill.select_by_value(f'{month_of_birth-1}')
+    month_fill.select_by_value(f'{MONTH_OF_BIRTH-1}')
     year_fill = Select(driver.find_element(By.CLASS_NAME, 'react-datepicker__year-select'))
-    year_fill.select_by_value(f'{year_of_birth}')
-    day_of_birth_fill = driver.find_element(By.CLASS_NAME, f'react-datepicker__day--0{day_of_birth}')
-    day_of_birth_fill.click()
+    year_fill.select_by_value(f'{YEAR_OF_BIRTH}')
+    day_fill = driver.find_element(By.CLASS_NAME, f'react-datepicker__day--0{DAY_OF_BIRTH}')
+    day_fill.click()
 
-    subject_fill = driver.find_element(By.XPATH, '//div[@class="subjects-auto-complete__input"]/input')
-    subject_fill.send_keys(subject)
+    subject_fill = driver.find_element(By.XPATH,
+                                       '//div[@class="subjects-auto-complete__input"]/input')
+    subject_fill.send_keys(SUBJECT)
     subject_fill.send_keys(Keys.ENTER)
 
     hobbies_fill = driver.find_elements(By.XPATH, '//label[@class="custom-control-label"]')
     hobbies_fill[sports].click()
 
-    current_address_fill = driver.find_element(By.CSS_SELECTOR, 'textarea[placeholder="Current Address"]')
-    current_address_fill.send_keys(current_address)
+    current_address_fill = driver.find_element(By.CSS_SELECTOR,
+                                               'textarea[placeholder="Current Address"]')
+    current_address_fill.send_keys(ADDRESS)
 
-    file_uploading = driver.find_element(By.ID, 'uploadPicture').find_element(By.XPATH, "//input[@type='file']")
+    file_uploading = driver.find_element(By.ID, 'uploadPicture')
+    file_uploading = file_uploading.find_element(By.XPATH, "//input[@type='file']")
     file_uploading.send_keys("/home/tm/Downloads/me.jpeg")
-
     file_uploading.submit()
 
 
