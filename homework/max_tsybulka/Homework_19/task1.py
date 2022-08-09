c86b93aad3a5e9af0c2a6a812bd74e4c3c7dc86c
maksim tsybulka <maksim.pvo@gmail.com>
2022-08-09 23:18:10 +0300

    delete left import

--- homework/max_tsybulka/Homework_19/task1.py
+++ homework/max_tsybulka/Homework_19/task1.py
@@ -1,19 +1,10 @@
-from audioop import add
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
-from time import sleep
 
 
-class Test():
+class Test:
     def setup_method(self):
         self.driver = webdriver.Chrome()
-        self.vars = {}
 
     def teardown_method(self):
         self.driver.quit()
