ed6866b6592028b067140ffeaf41d8c6347419fd
stassia92 <stassia92@mail.ru>
2022-08-11 11:07:15 +0300

    task1 fixed

--- homework/Anastacia_Kuznetcova/homework20/homework20_1.py
+++ homework/Anastacia_Kuznetcova/homework20/homework20_1.py
@@ -4,9 +4,23 @@ from selenium.webdriver.common.by import By
 
 serv = Service(executable_path=r'D:\dz1\prog\chromedriver.exe')
 Chrome_driver = webdriver.Chrome(service=serv)
-Chrome_driver.get("https://demoqa.com/checkbox")
-all_checkboxes = Chrome_driver.find_element(By.CLASS_NAME, "rct-checkbox")
-all_checkboxes.click()
-expend_all = Chrome_driver.find_element(By.CSS_SELECTOR, '#tree-node > div >\n'
-                                                         ' button.rct-option.rct-option-expand-all')
-expend_all.click()
+Chrome_driver.maximize_window()
+
+
+def click_checkboxes(chrome_driver):
+    chrome_driver.get('https://demoqa.com/checkbox')
+
+    for toggle_part_1 in chrome_driver.find_elements(By.CLASS_NAME, 'rct-collapse-btn'):
+        toggle_part_1.click()
+    for toggle_part_2 in chrome_driver.find_elements(By.CLASS_NAME,
+                                                     'rct-icon-expand-close'):
+        toggle_part_2.click()
+    for toggle_part_3 in chrome_driver.find_elements(By.CLASS_NAME, 'rct-icon-expand-close'):
+        toggle_part_3.click()
+
+    for checkbox_click in chrome_driver.find_elements(By.XPATH, '//span[@class="rct-checkbox"]'):
+        checkbox_click.click()
+        break
+
+
+click_checkboxes(Chrome_driver)
