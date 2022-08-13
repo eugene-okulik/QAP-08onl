66d36a37bc73866a485580c7f8a2a4381506af9d
Varvara <kirylchyk@gmail.com>
2022-08-12 23:19:18 -0400

    updates

--- homework/varvara_kirylchyk/varvara_kirylchyk_homework20/varvara_kirylchyk_homework20_1.py
+++ homework/varvara_kirylchyk/varvara_kirylchyk_homework20/varvara_kirylchyk_homework20_1.py
@@ -3,10 +3,36 @@
 # Их там 17. Открываются по клику на >
 # https://demoqa.com/checkbox
 
+from time import sleep
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Safari()
-driver.get('https://demoqa.com/checkbox')
-toggle = driver.find_element(By.CLASS_NAME, 'rct-checkbox')
-toggle.click()
+
+options = Options()
+options.add_argument('window-size=640,960')
+chrome_driver = webdriver.Chrome(options=options)
+
+
+def click_checkbox(driver):
+    driver.get('https://demoqa.com/checkbox')
+    driver.execute_script("""
+        var l = document.getElementById("fixedban").remove();
+     """)
+
+    for first_partition_toggle in driver.find_elements(By.CLASS_NAME, 'rct-collapse-btn'):
+        first_partition_toggle.click()
+    for second_partition_toggle in driver.find_elements(By.CLASS_NAME, 'rct-icon-expand-close'):
+        second_partition_toggle.click()
+    for third_partition_toggle in driver.find_elements(By.CLASS_NAME, 'rct-icon-expand-close'):
+        third_partition_toggle.click()
+    for checkbox in driver.find_elements(By.CLASS_NAME, 'rct-checkbox'):
+        checkbox.click()
+        break
+
+    sleep(3)
+    driver.quit()
+
+
+click_checkbox(chrome_driver)
