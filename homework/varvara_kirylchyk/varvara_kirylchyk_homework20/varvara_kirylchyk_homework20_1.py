2fd85ee35fc4d09d1977576c9a967cb28eb8f94a
Varvara <kirylchyk@gmail.com>
2022-08-12 23:28:26 -0400

    updates

--- homework/varvara_kirylchyk/varvara_kirylchyk_homework20/varvara_kirylchyk_homework20_1.py
+++ homework/varvara_kirylchyk/varvara_kirylchyk_homework20/varvara_kirylchyk_homework20_1.py
@@ -6,7 +6,6 @@
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 
