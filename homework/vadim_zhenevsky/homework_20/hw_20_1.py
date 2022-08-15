b1613781435cca558fa772d6c42bfa3a20e47388
Vadim Zhenevsky <zhenevskyvadim@gmail.com>
2022-08-15 21:33:45 +0300

    Homework_20 done

--- homework/vadim_zhenevsky/homework_20/hw_20_1.py
+++ homework/vadim_zhenevsky/homework_20/hw_20_1.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
+
 serv = Service(executable_path='chromedriver.exe')
 Chrome_driver = webdriver.Chrome()
 Chrome_driver.maximize_window()
