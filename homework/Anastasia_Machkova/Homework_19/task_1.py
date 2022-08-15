07a20f1df67aadc0763635cfab21ad7e50d7213b
Your Name <you@example.com>
2022-08-15 23:32:43 +0300

    Wrong import order fixed

--- homework/Anastasia_Machkova/Homework_19/task_1.py
+++ homework/Anastasia_Machkova/Homework_19/task_1.py
@@ -1,8 +1,8 @@
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from time import sleep
 from selenium.webdriver.support.ui import Select
 
 service = Service(executable_path='chromedriver.exe')
