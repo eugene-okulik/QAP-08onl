2dcd1a0db2394984a169c28b5be05e9dc3ca6063
SiarheiZhachek <SiarheiZhachek@yandex.ru>
2022-08-16 16:14:00 +0300

    sergey_zhachek_homework_21_done_fixed_pylint

--- homework/siarhei_zhachek/homework_21/test_mini_test.py
+++ homework/siarhei_zhachek/homework_21/test_mini_test.py
@@ -1,62 +1,55 @@
-import pytest
 import math
 from datetime import datetime
+import pytest
 
 
-@pytest.fixture(scope='session')
-def print_text():
-    print('\nСейчас будет много тестов)\n')
-    yield None
-    print('\nСпасибо за внимание!\n')
+class TestMini:
 
+    # pylint: disable=unused-argument, no-self-use
 
-@pytest.fixture(scope='function')
-def next_text():
-    print('\nЭта нанало теста.\n')
-    yield None
-    print('\nА вот это конец.\n')
+    @pytest.fixture(scope='session')
+    def print_text(self):
+        print('\nСейчас будет много тестов)\n')
+        yield None
+        print('\nСпасибо за внимание!\n')
 
+    @pytest.fixture(scope='function')
+    def next_text(self):
+        print('\nЭта нанало теста.\n')
+        yield None
+        print('\nА вот это конец.\n')
 
-@pytest.mark.simple
-def test_sum(print_text, next_text):
-    assert 5+5 == 10, 'Результат не соответствует ожидаемому'
+    @pytest.mark.simple
+    def test_sum(self, print_text, next_text):
+        assert 5+5 == 10, 'Результат не соответствует ожидаемому'
 
+    @pytest.mark.parametrize('result', [3, 2, 11, 5, 25])
+    def test_diff(self, result, next_text, print_text):
+        assert 5-2 == result, 'Результат не соответствует ожидаемому'
 
-@pytest.mark.parametrize('result', [3, 2, 11, 5, 25])
-def test_diff(result, next_text, print_text):
-    assert 5-2 == result, 'Результат не соответствует ожидаемому'
+    def test_product(self, print_text, next_text):
+        assert 5*3 == 15, 'Результат не соответствует ожидаемому'
 
+    def test_quotient(self, print_text, next_text):
+        assert 20/2 == 10, 'Результат не соответствует ожидаемому'
 
-def test_product(print_text, next_text):
-    assert 5*3 == 15, 'Результат не соответствует ожидаемому'
+    def test_equation(self, print_text, next_text):
+        assert 20/2+5 == 15, 'Результат не соответствует ожидаемому'
 
+    def test_less(self, print_text, next_text):
+        assert 9 < 11, 'Результат не соответствует ожидаемому'
 
-def test_quotient(print_text, next_text):
-    assert 20/2 == 10, 'Результат не соответствует ожидаемому'
+    @pytest.mark.skip('Bag - #325')
+    def test_more(self, print_text, next_text):
+        assert 7 > 5, 'Результат не соответствует ожидаемому'
 
+    @pytest.mark.skipif(datetime.now().month == 8, reason='в Августе корень не извлекаем')
+    def test_sqrt(self, print_text, next_text):
+        assert math.sqrt(9) == 3, 'Результат не соответствует ожидаемому'
 
-def test_text(print_text, next_text):
-    assert 'abc' == 'abc', 'Результат не соответствует ожидаемому'
+    def test_sqr(self, print_text, next_text):
+        assert 5 ** 2 == 25, 'Результат не соответствует ожидаемому'
 
-
-def test_less(print_text, next_text):
-    assert 9 < 11, 'Результат не соответствует ожидаемому'
-
-
-@pytest.mark.skip('Bag - #325')
-def test_more(print_text, next_text):
-    assert 7 > 5, 'Результат не соответствует ожидаемому'
-
-
-@pytest.mark.skipif(datetime.now().month == 8, reason='в Августе корень не извлекаем')
-def test_sqrt(print_text, next_text):
-    assert math.sqrt(9) == 3, 'Результат не соответствует ожидаемому'
-
-
-def test_sqr(print_text, next_text):
-    assert 5 ** 2 == 25, 'Результат не соответствует ожидаемому'
-
-
-@pytest.mark.hard
-def test_divide_without_a_remainder(print_text, next_text):
-    assert 1254 % 10 == 4, 'Результат не соответствует ожидаемому'
+    @pytest.mark.hard
+    def test_divide_without_a_remainder(self, print_text, next_text):
+        assert 1254 % 10 == 4, 'Результат не соответствует ожидаемому'
--- homework/siarhei_zhachek/homework_21/test_task_1.py
+++ homework/siarhei_zhachek/homework_21/test_task_1.py
@@ -7,109 +7,108 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.action_chains import ActionChains
 
 
-@pytest.fixture(scope='function')
-def driver():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.implicitly_wait(10)
-    yield driver
-    driver.quit()
+class TestTask:
 
+    # pylint: disable=unused-argument
 
-def test_copyright_info(driver):
-    driver.get('http://automationpractice.com/')
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-    copyright1 = driver.find_element(By.XPATH, '//section[@class="bottom-footer col-xs-12"]')
-    assert "© 2014 Ecommerce software by PrestaShop™" in copyright1.text
+    @pytest.fixture(scope='function')
+    def driver(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(10)
+        yield self.driver
+        self.driver.quit()
 
+    def test_copyright_info(self, driver):
+        self.driver.get('http://automationpractice.com/')
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+        copyright1 = self.driver.find_element(By.XPATH,
+                                              '//section[@class="bottom-footer col-xs-12"]')
+        assert "© 2014 Ecommerce software by PrestaShop™" in copyright1.text
 
-def test_check_logo_on_page_women(driver):
-    driver.get('http://automationpractice.com')
-    page_women = driver.find_element(By.XPATH, '//a[@title="Women"]')
-    page_women.click()
-    logo = driver.find_element(By.XPATH, '//img[@src="http://automationpractice.com/img/logo.jpg"]')
-    assert logo
+    def test_check_logo_on_page_women(self, driver):
+        self.driver.get('http://automationpractice.com')
+        page_women = self.driver.find_element(By.XPATH, '//a[@title="Women"]')
+        page_women.click()
+        logo = self.driver.find_element(By.XPATH,
+                                        '//img[@src="http://automationpractice.com/img/logo.jpg"]')
+        assert logo
 
+    def test_check_logo_on_page_dresses(self, driver):
+        self.driver.get('http://automationpractice.com')
+        page_dresses = self.driver.find_element(By.XPATH, '//div[@id="block_top_menu"]/ul/li[2]/a')
+        page_dresses.click()
+        logo = self.driver.find_element(By.XPATH,
+                                        '//img[@src="http://automationpractice.com/img/logo.jpg"]')
+        assert logo
 
-def test_check_logo_on_page_dresses(driver):
-    driver.get('http://automationpractice.com')
-    page_dresses = driver.find_element(By.XPATH, '//div[@id="block_top_menu"]/ul/li[2]/a')
-    page_dresses.click()
-    logo = driver.find_element(By.XPATH, '//img[@src="http://automationpractice.com/img/logo.jpg"]')
-    assert logo
+    def test_check_logo_on_page_t_shirts(self, driver):
+        self.driver.get('http://automationpractice.com')
+        page_dresses = self.driver.find_element(By.XPATH, '//div[@id="block_top_menu"]/ul/li[3]/a')
+        page_dresses.click()
+        logo = self.driver.find_element(By.XPATH,
+                                        '//img[@src="http://automationpractice.com/img/logo.jpg"]')
+        assert logo
 
+    def test_invalid_email_address(self, driver):
+        self.driver.get('http://automationpractice.com/')
+        sing_in = self.driver.find_element(By.XPATH, '//a[@class="login"]')
+        sing_in.click()
+        email_addres = self.driver.find_element(By.ID, 'email_create')
+        email_addres.send_keys('мыло')
+        button = self.driver.find_element(By.ID, 'SubmitCreate')
+        button.click()
+        alert_danger = self.driver.find_element(By.CLASS_NAME, 'alert-danger')
+        alert_danger_text = alert_danger.find_element(By.TAG_NAME, 'li')
+        assert "Invalid email address." in alert_danger_text.text
 
-def test_check_logo_on_page_t_shirts(driver):
-    driver.get('http://automationpractice.com')
-    page_dresses = driver.find_element(By.XPATH, '//div[@id="block_top_menu"]/ul/li[3]/a')
-    page_dresses.click()
-    logo = driver.find_element(By.XPATH, '//img[@src="http://automationpractice.com/img/logo.jpg"]')
-    assert logo
+    def test_alphabetical_sorting_and_quality_control(self, driver):
+        self.driver.get('http://automationpractice.com/index.php?id_category=3&controller=category')
+        quantity = self.driver.find_element(By.CLASS_NAME, 'product-count')
+        sort_alphabetically = Select(self.driver.find_element(By.ID, 'selectProductSort'))
+        sort_alphabetically.select_by_value('name:asc')
+        quantity_after_sort = self.driver.find_element(By.CLASS_NAME, 'product-count')
+        assert quantity_after_sort.text == quantity.text
 
+    def test_add_to_cart(self, driver):
+        self.driver.get('http://automationpractice.com/index.php?id_category=3&controller=category')
+        search_product = self.driver.find_element(By.LINK_TEXT, 'Printed Dress')
+        actions = ActionChains(self.driver)
+        actions.move_to_element(search_product)
+        actions.perform()
+        add_product_1 = self.driver.find_element(By.XPATH, '//a[@data-id-product="3"]')
+        add_product_1.click()
+        close_window = self.driver.find_element(By.XPATH, '//span[@class="cross"]')
+        close_window.click()
+        cart_after_add = self.driver.find_element(By.XPATH, '//a[@title="View my shopping cart"]')
+        assert 'Cart 1 Product' in cart_after_add.text
 
-def test_invalid_email_address(driver):
-    driver.get('http://automationpractice.com/')
-    sing_in = driver.find_element(By.XPATH, '//a[@class="login"]')
-    sing_in.click()
-    email_addres = driver.find_element(By.ID, 'email_create')
-    email_addres.send_keys('мыло')
-    button = driver.find_element(By.ID, 'SubmitCreate')
-    button.click()
-    alert_danger = driver.find_element(By.CLASS_NAME, 'alert-danger')
-    alert_danger_text = alert_danger.find_element(By.TAG_NAME, 'li')
-    assert "Invalid email address." in alert_danger_text.text
+    def test_button_click(self, driver):
+        self.driver.get('https://testpages.herokuapp.com/styled/dynamic-buttons-simple.html')
+        button_start = self.driver.find_element(By.ID, 'button00')
+        button_start.click()
+        button_one = self.driver.find_element(By.ID, 'button01')
+        button_one.click()
+        button_two = self.driver.find_element(By.ID, 'button02')
+        button_two.click()
+        button_three = self.driver.find_element(By.ID, 'button03')
+        button_three.click()
+        button_check = self.driver.find_element(By.ID, 'buttonmessage')
+        assert 'All Buttons Clicked' in button_check.text
 
-
-def test_alphabetical_sorting_and_quality_control(driver):
-    driver.get('http://automationpractice.com/index.php?id_category=3&controller=category')
-    quantity = driver.find_element(By.CLASS_NAME, 'product-count')
-    sort_alphabetically = Select(driver.find_element(By.ID, 'selectProductSort'))
-    sort_alphabetically.select_by_value('name:asc')
-    quantity_after_sort = driver.find_element(By.CLASS_NAME, 'product-count')
-    assert quantity_after_sort.text == quantity.text
-
-
-def test_add_to_cart(driver):
-    driver.get('http://automationpractice.com/index.php?id_category=3&controller=category')
-    search_product = driver.find_element(By.LINK_TEXT, 'Printed Dress')
-    actions = ActionChains(driver)
-    actions.move_to_element(search_product)
-    actions.perform()
-    add_product_1 = driver.find_element(By.XPATH, '//a[@data-id-product="3"]')
-    add_product_1.click()
-    close_window = driver.find_element(By.XPATH, '//span[@class="cross"]')
-    close_window.click()
-    cart_after_add = driver.find_element(By.XPATH, '//a[@title="View my shopping cart"]')
-    assert 'Cart 1 Product' in cart_after_add.text
-
-
-def test_button_click(driver):
-    driver.get('https://testpages.herokuapp.com/styled/dynamic-buttons-simple.html')
-    button_start = driver.find_element(By.ID, 'button00')
-    button_start.click()
-    button_one = driver.find_element(By.ID, 'button01')
-    button_one.click()
-    button_two = driver.find_element(By.ID, 'button02')
-    button_two.click()
-    button_three = driver.find_element(By.ID, 'button03')
-    button_three.click()
-    button_check = driver.find_element(By.ID, 'buttonmessage')
-    assert 'All Buttons Clicked' in button_check.text
-
-
-def test_button_click_task_2(driver):
-    driver.get('https://testpages.herokuapp.com/styled/dynamic-buttons-simple.html')
-    button_start = driver.find_element(By.ID, 'button00')
-    WebDriverWait(driver, 10).until(ec.element_to_be_clickable(button_start))
-    button_start.click()
-    button_one = driver.find_element(By.ID, 'button01')
-    WebDriverWait(driver, 10).until(ec.element_to_be_clickable(button_one))
-    button_one.click()
-    button_two = driver.find_element(By.ID, 'button02')
-    WebDriverWait(driver, 10).until(ec.element_to_be_clickable(button_two))
-    button_two.click()
-    button_three = driver.find_element(By.ID, 'button03')
-    WebDriverWait(driver, 10).until(ec.element_to_be_clickable(button_three))
-    button_three.click()
-    button_check = driver.find_element(By.ID, 'buttonmessage')
-    assert 'All Buttons Clicked' in button_check.text
+    def test_button_click_task_2(self, driver):
+        self.driver.get('https://testpages.herokuapp.com/styled/dynamic-buttons-simple.html')
+        button_start = self.driver.find_element(By.ID, 'button00')
+        WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable(button_start))
+        button_start.click()
+        button_one = self.driver.find_element(By.ID, 'button01')
+        WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable(button_one))
+        button_one.click()
+        button_two = self.driver.find_element(By.ID, 'button02')
+        WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable(button_two))
+        button_two.click()
+        button_three = self.driver.find_element(By.ID, 'button03')
+        WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable(button_three))
+        button_three.click()
+        button_check = self.driver.find_element(By.ID, 'buttonmessage')
+        assert 'All Buttons Clicked' in button_check.text
