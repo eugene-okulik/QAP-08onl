c2ad8dfab6d263624e885247e3ac8c0e86c27156
tatiana molofei <tmolofey@yandex.by>
2022-08-17 22:31:26 +0300

    Added Task_1 from Homework_21

--- homework/tatiana_molofei/Homework_21/test_hw_21_01.py
+++ homework/tatiana_molofei/Homework_21/test_hw_21_01.py
@@ -16,59 +16,68 @@ def print_all_tests():
     print("\nAfter every test\n")
 
 
-@pytest.mark.usefixtures()
+@pytest.mark.usefixtures
 @pytest.mark.simple
 def test_sum(print_begin_end, print_all):
     assert 2+2 == 4
 
 
-@pytest.mark.usefixtures()
+@pytest.mark.usefixtures
 @pytest.mark.simple
 def test_dif(print_all):
     assert 5-2 == 3
 
 
+@pytest.mark.usefixtures
 @pytest.mark.simple
 def test_mult(print_all):
     assert 4*3 == 12
 
 
+@pytest.mark.usefixtures
 @pytest.mark.hard
 def test_div(print_all):
     assert 4 / 2 == 2
 
 
+@pytest.mark.usefixtures
 @pytest.mark.simple
 def test_isalpha(print_all):
     assert "helloMyFriend".isalpha()
 
 
+@pytest.mark.usefixtures
 @pytest.mark.hard
 def test_isdigit(print_all):
     assert "34566".isdigit()
 
 
+@pytest.mark.usefixtures
 @pytest.mark.hard
 @pytest.mark.skip("Useless test")
 def test_entry(print_all):
     assert 'a' in "Tryam"
 
 
+@pytest.mark.usefixtures
 @pytest.mark.hard
 def test_comp(print_all):
     assert 6 < 8
 
 
+@pytest.mark.usefixtures
 @pytest.mark.hard
 def test_pow(print_all):
     assert 2 ** 4 == 16
 
 
+@pytest.mark.usefixtures
 @pytest.mark.hard
 def test_round(print_all):
     assert math.ceil(8.999) == 9
 
 
+@pytest.mark.usefixtures
 @pytest.mark.hard
 @pytest.mark.parametrize('items', [5, 7])
 def test_factorial(print_all, items):
