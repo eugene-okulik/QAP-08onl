712267076a2d5db3e4d2b99124f3bceeef0c49aa
Sergey1991wolk <svwolk1991@gmail.com>
2022-08-18 19:14:41 +0300

    Wolk_Sergey_homework_21

--- homework/wolk/wolk_sergey_homework_21/mini_tests.py
+++ homework/wolk/wolk_sergey_homework_21/mini_tests.py
@@ -18,45 +18,45 @@ def prt_text():
 
 
 @pytest.simple.test
-def test_sum(print_text):
+def test_sum():
     assert 2+2 == 4, 'Wrong result'
 
 
 @pytest.difficult.test('result', [8, 2, 12, 5, 21])
-def test_diff(result, print_text):
+def test_diff(result):
     assert 10-2 == result, 'Wrong result'
 
 
-def test_multiplication(print_text):
+def test_multiplication():
     assert 4*5 == 40, 'Wrong result'
 
 
-def test_division(print_text):
+def test_division():
     assert 10/2 == 5, 'Wrong result'
 
 
-def test_abcc(print_text):
+def test_abcc():
     assert 'abc' == 'abc', 'Wrong result'
 
 
-def test_differen(print_text):
+def test_differen():
     assert 4 < 8, 'Wrong result'
 
 
 @pytest.test.skip('Bag - #325')
-def test_more(print_text):
+def test_more():
     assert 7 > 5, 'Wrong result'
 
 
 @pytest.sqrt.skip(datetime.now().month == 12, reason='We do not extract the root at the number 12')
-def test_sqrt(print_text):
+def test_sqrt():
     assert math.sqrt(9) == 3, 'Wrong result'
 
 
-def test_sqr(print_text):
+def test_sqr():
     assert 5 ** 2 == 25, 'Wrong result'
 
 
 @pytest.test.div
-def test_percent(print_text):
+def test_percent():
     assert 1254 % 10 == 4, 'Wrong result'
