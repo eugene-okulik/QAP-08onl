a30265affe9eec57fd8d57cad97d558412151403
Artyom Zabello <a.zabela@andersenlab.com>
2022-08-19 21:11:37 +0300

    homework 21 fixes v2

--- homework/artyom_zabello/homework_21/tests/test_pytest_1.py
+++ homework/artyom_zabello/homework_21/tests/test_pytest_1.py
@@ -2,45 +2,62 @@ import pytest
 
 
 @pytest.mark.usefixtures('set_up', 'action')
-class TestSimple:
-    @pytest.mark.skip('Test is too simple')
-    def test1(self):
-        assert 1 + 5 == 6
+@pytest.mark.skip('Test is too simple')
+def test1():
+    assert 1 + 5 == 6
 
-    @pytest.mark.simple
-    def test2(self):
-        assert 5 - 2 == 6, "Фактический результат не соответствует ожидаемомоу"
 
-    @pytest.mark.simple
-    @pytest.mark.parametrize('x_1,y_1', [[9, 7], [11, 2]])
-    def test3(self, x_1, y_1):
-        assert x_1 > y_1, "Фактический результат не соответствует ожидаемомоу"
+@pytest.mark.usefixtures('set_up', 'action')
+@pytest.mark.simple
+def test2():
+    assert 5 - 2 == 6, "Фактический результат не соответствует ожидаемомоу"
 
-    @pytest.mark.simple
-    def test4(self):
-        assert 3 + 3 == 6
 
-    @pytest.mark.simple
-    def test5(self):
-        assert 4 - 1 == 3
+@pytest.mark.usefixtures('set_up', 'action')
+@pytest.mark.simple
+@pytest.mark.parametrize('x_1,y_1', [[9, 7], [11, 2]])
+def test3(x_1, y_1):
+    assert x_1 > y_1, "Фактический результат не соответствует ожидаемомоу"
 
-    @pytest.mark.simple
-    def test6(self):
-        assert 4 / 2 == 2
 
-    @pytest.mark.simple
-    def test7(self):
-        assert 4 + 5 == 9
+@pytest.mark.usefixtures('set_up', 'action')
+@pytest.mark.simple
+def test4():
+    assert 3 + 3 == 6
 
-    @pytest.mark.simple
-    def test8(self):
-        assert 5+5 == 10
 
-    @pytest.mark.simple
-    def test9(self):
-        assert 1+1 == 2
+@pytest.mark.usefixtures('set_up', 'action')
+@pytest.mark.simple
+def test5():
+    assert 4 - 1 == 3
 
-    @pytest.mark.simple
-    def test10(self):
-        res11 = 10 + 20
-        assert res11 == 30
+
+@pytest.mark.usefixtures('set_up', 'action')
+@pytest.mark.simple
+def test6():
+    assert 4 / 2 == 2
+
+
+@pytest.mark.usefixtures('set_up', 'action')
+@pytest.mark.simple
+def test7():
+    assert 4 + 5 == 9
+
+
+@pytest.mark.usefixtures('set_up', 'action')
+@pytest.mark.simple
+def test8():
+    assert 5+5 == 10
+
+
+@pytest.mark.usefixtures('set_up', 'action')
+@pytest.mark.simple
+def test9():
+    assert 1+1 == 2
+
+
+@pytest.mark.usefixtures('set_up', 'action')
+@pytest.mark.simple
+def test10():
+    res11 = 10 + 20
+    assert res11 == 30
