91fc3781b0bead65849eeb4faf707138a4b2c7be
NikolayNazarov <nikolaynazarovvv216@gmail.com>
2022-08-21 18:53:26 +0300

    homework_21 second commit

--- homework/nikolay_nazarov/Homework_21/task_2.py
+++ homework/nikolay_nazarov/Homework_21/task_2.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -16,16 +18,43 @@ def driver():
     driver.quit()
 
 
-def test_blank_text(driver):
-    driver.get("http://automationpractice.com/")
-    driver.maximize_window()
-    blank_text = driver.find_element(By.CLASS_NAME, "_blank").text
-    assert blank_text == "Ecommerce software by PrestaShop™", "В самом низу главной страницы нет текста " \
-                                                              "Ecommerce software by PrestaShop™"
+class TestTask:
 
-def test_screen_is_visible(driver):
-    driver.get("http://automationpractice.com/")
-    driver.maximize_window()
-    blank_text = driver.find_element(By.CSS_SELECTOR, "[alt="My Store"]").src
-    assert blank_text == "Ecommerce software by PrestaShop™", "В самом низу главной страницы нет текста " \
-                                                              "Ecommerce software by PrestaShop™"
+    @pytest.fixture(scope='function')
+    def driver(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(10)
+        yield
+        self.driver.quit()
+
+    @pytest.mark.skip
+    @pytest.mark.usefixtures('driver')
+    def test_blank_text(self):
+        self.driver.get("http://automationpractice.com/")
+        self.driver.maximize_window()
+        blank_text = self.driver.find_element(By.CLASS_NAME, "_blank").text
+        assert blank_text == "Ecommerce software by PrestaShop™", "В самом низу главной страницы нет текста " \
+                                                                  "Ecommerce software by PrestaShop™"
+
+    @pytest.mark.usefixtures('driver')
+    def test_logo_is_visible(self):
+        self.driver.get("http://automationpractice.com/")
+        self.driver.maximize_window()
+        print("1")
+        logo = self.driver.find_element(By.CSS_SELECTOR, "[alt='My Store']")
+        assert logo, "Нет лого на странице"
+        print("2")
+        self.driver.find_element(By.CSS_SELECTOR, "[title = 'Women']").click()
+        logo = self.driver.find_element(By.CSS_SELECTOR, "[alt='My Store']")
+        assert logo, "Нет лого на странице"
+        print("3")
+        self.driver.find_element(By.CSS_SELECTOR, "[title = 'Dresses']").click()
+        logo = self.driver.find_element(By.CSS_SELECTOR, "[alt='My Store']")
+        assert logo, "Нет лого на странице"
+        print("4")
+        self.driver.find_element(By.CSS_SELECTOR, "[title = 'T-shirts']").click()
+        logo = self.driver.find_element(By.CSS_SELECTOR, "[alt='My Store']")
+        assert logo, "Нет лого на странице"
+
+
