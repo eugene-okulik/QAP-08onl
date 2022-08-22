b1ee2b4dd4564193ba06dd3ebc74929c740493ae
eugeny.okulik <tiburon854@gmail.com>
2022-08-22 22:03:02 +0300

    POM project

---
+++ homework/eugeny_okulik/pom/tests/test_login_page.py
@@ -0,0 +1,19 @@
+from pom.pages.home_page import HomePage
+from pom.pages.authentication import AuthPage
+
+
+def test_login(driver):
+    home_page = HomePage(driver)
+    home_page.open()
+    auth_page = home_page.open_sign_in()
+    # auth_page = AuthPage(driver)
+    assert auth_page.is_displayed_email_field()
+
+
+def test_passwd(driver):
+    home_page = HomePage(driver)
+    home_page.open()
+    home_page.sign_in_button.click()
+    # home_page.open_sign_in()
+    auth_page = AuthPage(driver)
+    assert auth_page.passwd_field.is_displayed()
