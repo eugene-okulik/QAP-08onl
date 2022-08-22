67eded868ad6a245620dbb19a003f3b1ed6326ef
Sergey1991wolk <svwolk1991@gmail.com>
2022-08-22 15:50:59 +0300

    Wolk_Sergey_homework_21

--- homework/wolk/wolk_sergey_homework_21/homework_tasks.py
+++ homework/wolk/wolk_sergey_homework_21/homework_tasks.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 
 @pytest.fixture(scope='function')
-def driver() -> None:
+def driver_1() -> None:
     #service = Service(executable_path='chromedriver.exe')
     options = Options()
     options.add_argument('start-maximized')
@@ -20,87 +20,87 @@ def driver() -> None:
 
 
 def test_check():
-    driver.get('http://automationpractice.com')
-    page_women = driver.find_element(By.XPATH, '//a[@title="Women"]')
+    driver_1.get('http://automationpractice.com')
+    page_women = driver_1.find_element(By.XPATH, '//a[@title="Women"]')
     page_women.click()
-    logo = driver.find_element(By.XPATH, '//img[@src="http://automationpractice.com/img/logo.jpg"]')
+    logo = driver_1.find_element(By.XPATH, '//img[@src="http://automationpractice.com/img/logo.jpg"]')
     assert logo
 
 
 def test_page_dresses():
-    driver.get('http://automationpractice.com')
-    page_dresses = driver.find_element(By.XPATH, '//div[@id="block_top_menu"]/ul/li[2]/a')
+    driver_1.get('http://automationpractice.com')
+    page_dresses = driver_1.find_element(By.XPATH, '//div[@id="block_top_menu"]/ul/li[2]/a')
     page_dresses.click()
-    logo = driver.find_element(By.XPATH, '//img[@src="http://automationpractice.com/img/logo.jpg"]')
+    logo = driver_1.find_element(By.XPATH, '//img[@src="http://automationpractice.com/img/logo.jpg"]')
     assert logo
 
 
 def test_shirts():
-    driver.get('http://automationpractice.com')
-    page_dresses = driver.find_element(By.XPATH, '//div[@id="block_top_menu"]/ul/li[3]/a')
+    driver_1.get('http://automationpractice.com')
+    page_dresses = driver_1.find_element(By.XPATH, '//div[@id="block_top_menu"]/ul/li[3]/a')
     page_dresses.click()
-    logo = driver.find_element(By.XPATH, '//img[@src="http://automationpractice.com/img/logo.jpg"]')
+    logo = driver_1.find_element(By.XPATH, '//img[@src="http://automationpractice.com/img/logo.jpg"]')
     assert logo
 
 def test_email():
-    driver.get('http://automationpractice.com/')
-    sing_in = driver.find_element(By.XPATH, '//a[@class="login"]')
+    driver_1.get('http://automationpractice.com/')
+    sing_in = driver_1.find_element(By.XPATH, '//a[@class="login"]')
     sing_in.click()
-    email_addres = driver.find_element(By.ID, 'email_create')
+    email_addres = driver_1.find_element(By.ID, 'email_create')
     email_addres.send_keys('email')
-    button = driver.find_element(By.ID, 'SubmitCreate')
+    button = driver_1.find_element(By.ID, 'SubmitCreate')
     button.click()
-    alert_danger = driver.find_element(By.CLASS_NAME, 'alert-danger')
+    alert_danger = driver_1.find_element(By.CLASS_NAME, 'alert-danger')
     alert_danger_text = alert_danger.find_element(By.TAG_NAME, 'li')
     assert "Invalid email address." in alert_danger_text.text
 
 def test_quality_control():
-    driver.get('http://automationpractice.com/index.php?id_category=3&controller=category')
-    quantity = driver.find_element(By.CLASS_NAME, 'product-count')
-    sort_alphabetically = Select(driver.find_element(By.ID, 'selectProductSort'))
+    driver_1.get('http://automationpractice.com/index.php?id_category=3&controller=category')
+    quantity = driver_1.find_element(By.CLASS_NAME, 'product-count')
+    sort_alphabetically = Select(driver_1.find_element(By.ID, 'selectProductSort'))
     sort_alphabetically.select_by_value('name:asc')
-    quantity_after_sort = driver.find_element(By.CLASS_NAME, 'product-count')
+    quantity_after_sort = driver_1.find_element(By.CLASS_NAME, 'product-count')
     assert quantity_after_sort.text == quantity.text
 
 def test_cart():
-    driver.get('http://automationpractice.com/index.php?id_category=3&controller=category')
-    search_product = driver.find_element(By.LINK_TEXT, 'Printed Dress')
-    actions = ActionChains(driver)
+    driver_1.get('http://automationpractice.com/index.php?id_category=3&controller=category')
+    search_product = driver_1.find_element(By.LINK_TEXT, 'Printed Dress')
+    actions = ActionChains(driver_1)
     actions.move_to_element(search_product)
     actions.perform()
-    add_product_1 = driver.find_element(By.XPATH, '//a[@data-id-product="3"]')
+    add_product_1 = driver_1.find_element(By.XPATH, '//a[@data-id-product="3"]')
     add_product_1.click()
-    close_window = driver.find_element(By.XPATH, '//span[@class="cross"]')
+    close_window = driver_1.find_element(By.XPATH, '//span[@class="cross"]')
     close_window.click()
-    cart_after_add = driver.find_element(By.XPATH, '//a[@title="View my shopping cart"]')
+    cart_after_add = driver_1.find_element(By.XPATH, '//a[@title="View my shopping cart"]')
     assert 'Cart 1 Product' in cart_after_add.text
 
 def test_but_click():
-    driver.get('https://testpages.herokuapp.com/styled/dynamic-buttons-simple.html')
-    button_start = driver.find_element(By.ID, 'button00')
+    driver_1.get('https://testpages.herokuapp.com/styled/dynamic-buttons-simple.html')
+    button_start = driver_1.find_element(By.ID, 'button00')
     button_start.click()
-    button_one = driver.find_element(By.ID, 'button01')
+    button_one = driver_1.find_element(By.ID, 'button01')
     button_one.click()
-    button_two = driver.find_element(By.ID, 'button02')
+    button_two = driver_1.find_element(By.ID, 'button02')
     button_two.click()
-    button_three = driver.find_element(By.ID, 'button03')
+    button_three = driver_1.find_element(By.ID, 'button03')
     button_three.click()
-    button_check = driver.find_element(By.ID, 'buttonmessage')
+    button_check = driver_1.find_element(By.ID, 'buttonmessage')
     assert 'All Buttons Clicked' in button_check.text
 
 def test_button_click_2():
-    driver.get('https://testpages.herokuapp.com/styled/dynamic-buttons-simple.html')
-    button_start = driver.find_element(By.ID, 'button00')
-    WebDriverWait(driver, 10).until(ec.element_to_be_clickable(button_start))
+    driver_1.get('https://testpages.herokuapp.com/styled/dynamic-buttons-simple.html')
+    button_start = driver_1.find_element(By.ID, 'button00')
+    WebDriverWait(driver_1, 10).until(ec.element_to_be_clickable(button_start))
     button_start.click()
-    button_one = driver.find_element(By.ID, 'button01')
-    WebDriverWait(driver, 10).until(ec.element_to_be_clickable(button_one))
+    button_one = driver_1.find_element(By.ID, 'button01')
+    WebDriverWait(driver_1, 10).until(ec.element_to_be_clickable(button_one))
     button_one.click()
-    button_two = driver.find_element(By.ID, 'button02')
-    WebDriverWait(driver, 10).until(ec.element_to_be_clickable(button_two))
+    button_two = driver_1.find_element(By.ID, 'button02')
+    WebDriverWait(driver_1, 10).until(ec.element_to_be_clickable(button_two))
     button_two.click()
-    button_three = driver.find_element(By.ID, 'button03')
-    WebDriverWait(driver, 10).until(ec.element_to_be_clickable(button_three))
+    button_three = driver_1.find_element(By.ID, 'button03')
+    WebDriverWait(driver_1, 10).until(ec.element_to_be_clickable(button_three))
     button_three.click()
-    button_check = driver.find_element(By.ID, 'buttonmessage')
+    button_check = driver_1.find_element(By.ID, 'buttonmessage')
     assert 'All Buttons Clicked' in button_check.text
