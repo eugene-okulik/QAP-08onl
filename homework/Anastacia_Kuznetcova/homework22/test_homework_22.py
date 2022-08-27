afe2adae8d0004bb6b94c36bdafdb9036eb8e92c
stassia92 <stassia92@mail.ru>
2022-08-27 13:56:54 +0300

    fixed

--- homework/Anastacia_Kuznetcova/homework22/test_homework_22.py
+++ homework/Anastacia_Kuznetcova/homework22/test_homework_22.py
@@ -1,4 +1,3 @@
-from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.common.action_chains import ActionChains
@@ -7,10 +6,10 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import Keys
 
 
-def test_1(driver):
+def test_check_cart_product(driver):
     driver.get('https://www.demoblaze.com/index.html')
-    goods = driver.find_elements(By.CLASS_NAME, 'hrefch')
-    goods[0].send_keys(Keys.LEFT_CONTROL, Keys.ENTER)
+    product_name = driver.find_elements(By.CLASS_NAME, 'hrefch')
+    product_name[0].send_keys(Keys.LEFT_CONTROL, Keys.ENTER)
     win = driver.window_handles
     driver.switch_to.window(win[1])
     driver.find_element(By.CSS_SELECTOR, 'a[onclick="addToCart(1)"]').click()
@@ -23,9 +22,8 @@ def test_1(driver):
     assert "Samsung galaxy s6" in cart.text
 
 
-def test_2(driver):
+def test_choose_element(driver):
     driver.get('https://demoqa.com/menu#')
-    sleep(1)
     main_item_2 = driver.find_element(By.LINK_TEXT, 'Main Item 2')
     ActionChains(driver).move_to_element(main_item_2).perform()
     sub_list = driver.find_element(By.LINK_TEXT, 'SUB SUB LIST »')
@@ -34,7 +32,7 @@ def test_2(driver):
     ActionChains(driver).move_to_element(sub_item_2).perform()
 
 
-def test_3(driver):
+def test_check_alert_txt(driver):
     driver.get('https://testpages.herokuapp.com/styled/alerts/alert-test.html')
     driver.find_element(By.ID, 'promptexample').click()
     Alert(driver).send_keys('Homework22')
