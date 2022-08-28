dd0b444cd3d63b8ef3b1bd65200f55af1617848e
NikolayNazarov <nikolaynazarovvv216@gmail.com>
2022-08-28 19:42:50 +0300

    homework_23 completed

--- homework/nikolay_nazarov/Homework_23/pom_nazarov/pages/locators/home_page_locators.py
+++ homework/nikolay_nazarov/Homework_23/pom_nazarov/pages/locators/home_page_locators.py
@@ -2,7 +2,8 @@ from selenium.webdriver.common.by import By
 
 
 sign_in_button = (By.CLASS_NAME, 'login')
-go_to_login_screen_button = (By.CSS_SELECTOR, 'div[class="header__userzone-image header__userzone-image--auth"]')
+go_to_login_screen_button = (By.CSS_SELECTOR,
+                             'div[class="header__userzone-image header__userzone-image--auth"]')
 confirm_address_button = (By.CSS_SELECTOR, '.b-location-confirmation__buttons-list button')
 login_input = (By.CSS_SELECTOR, 'input[placeholder="Телефон или электронная почта"]')
 password_input = (By.CSS_SELECTOR, 'input[placeholder="Пароль"]')
