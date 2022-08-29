1b9f2148b7cc225a43ed2a258e5302504699e118
stassia92 <stassia92@mail.ru>
2022-08-29 05:24:08 +0300

    fixed pylint

--- homework/Anastacia_Kuznetcova/homework23/pom/pages/locators/home_page_locators.py
+++ homework/Anastacia_Kuznetcova/homework23/pom/pages/locators/home_page_locators.py
@@ -2,5 +2,4 @@ from selenium.webdriver.common.by import By
 
 t_shirts_page = (By.XPATH, '//*[@id="block_top_menu"]/ul/li[3]')
 contact_us = (By.ID, "contact-link")
-cart = (By.CSS_SELECTOR,
-                                  '#header > div:nth-child(3) > div > div > div:nth-child(3) > div > a > b')
+cart = (By.CSS_SELECTOR, '#header > div:nth-child(3) > div > div > div:nth-child(3) > div > a > b')
