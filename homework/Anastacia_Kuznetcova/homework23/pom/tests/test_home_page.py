21968741c6844ff27ae9c7388948b4da72729a60
stassia92 <stassia92@mail.ru>
2022-08-29 05:41:53 +0300

    fixed v1

--- homework/Anastacia_Kuznetcova/homework23/pom/tests/test_home_page.py
+++ homework/Anastacia_Kuznetcova/homework23/pom/tests/test_home_page.py
@@ -18,5 +18,5 @@ def test_cart_is_displayed(driver):
 def test_t_shirt_txt(driver):
     home_page = HomePage(driver)
     home_page.open()
-    home_page.t_shirt_is_displayed()
-    assert home_page.cart_is_displayed()
+    home_page.t_shirt_btn()
+    assert home_page.t_shirt_is_displayed()
