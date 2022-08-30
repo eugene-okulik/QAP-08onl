50c1092e4207bf0a5b83f03e7735c4f26b76c6e0
Your Name <you@example.com>
2022-08-30 22:20:57 +0300

    Homework 23 done

---
+++ homework/Anastasia_Machkova/Homework_23/pom/pages/home_page.py
@@ -0,0 +1,47 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+search_field = (By.ID, 'search_query_top')
+search_button = (By.NAME, 'submit_search')
+alert_field = (By.CLASS_NAME, 'alert-warning')
+add_blouse = (By.CSS_SELECTOR, 'a[data-id-product="2"]')
+container = (By.CLASS_NAME, 'product-container')
+empty_text = (By.CLASS_NAME, 'ajax_cart_no_product')
+contact = (By.ID, 'contact-link')
+
+
+class HomePage(BasePage):
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver = driver
+
+    def open(self):
+        self.driver.get('http://automationpractice.com/index.php')
+
+    def contact_us(self):
+        return self.find_element(contact).click()
+
+    def move_to_blouse(self):
+        product = self.find_element(container)
+        return self.move_to_element(product).perform()
+
+    def add_blouse_to_cart(self):
+        return self.find_element(add_blouse).click()
+
+    def blouse_in_cart(self):
+        return self.find_element(empty_text).text
+
+    def search_field(self):
+        return self.find_element(search_field)
+
+    def enter_word(self, word):
+        search = self.find_element(search_field)
+        search.click()
+        search.send_keys(word)
+        return search_field
+
+    def click_on_the_search_button(self):
+        return self.find_element(search_button).click()
+
+    def alert(self):
+        return self.find_element(alert_field).is_displayed()
---
+++ homework/Anastasia_Machkova/pom/pages/authentification.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+email_field = (By.ID, 'email')
+password_field = (By.ID, 'passwd')
+
+
+class AuthPage(BasePage):
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver = driver
+
+    @property
+    def email_filed(self):
+        return self.find_element(email_field)
+
+    @property
+    def passwd_field(self):
+        return self.find_element(password_field)
+
+    def is_displayed_email_field(self):
+        return self.email_filed.is_displayed()
