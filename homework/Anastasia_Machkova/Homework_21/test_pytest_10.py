4d99efd5edebcadfcd7a539989191f31242cd931
Your Name <you@example.com>
2022-08-31 10:02:59 +0300

    Fixed fixtures

--- homework/Anastasia_Machkova/Homework_21/test_pytest_10.py
+++ homework/Anastasia_Machkova/Homework_21/test_pytest_10.py
@@ -15,34 +15,34 @@ def all_tests():
     print('\nTesting is finished\n')
 
 
-@pytest.mark.usefixtures(name='this_test')
+@pytest.mark.usefixtures('this_test', 'all_tests')
 @pytest.mark.simple
-def test_1(all_tests):
+def test_1():
     print('test_1')
     assert 2+2 == 4, 'The result is not as expected'
 
 
-@pytest.mark.usefixtures(name='this_test')
+@pytest.mark.usefixtures('this_test')
 @pytest.mark.simple
 def test_2():
     print('test_2')
     assert 3-1 == 2, 'The result is not as expected'
 
 
-@pytest.mark.usefixtures(name='this_test')
+@pytest.mark.usefixtures('this_test')
 def test_3():
     print('test_3')
     assert 11*11 == 121, 'The result is not as expected'
 
 
 @pytest.mark.skip(reason='Bug #3')
-@pytest.mark.usefixtures(name='this_test')
+@pytest.mark.usefixtures('this_test')
 def test_4():
     print('test_4')
     assert 10/5 == 2, 'The result is not as expected'
 
 
-@pytest.mark.usefixtures(name='this_test')
+@pytest.mark.usefixtures('this_test', 'all_tests')
 @pytest.mark.hard
 @pytest.mark.parametrize('x_num', [3, 4, 5])
 def test_5(x_num):
@@ -50,7 +50,7 @@ def test_5(x_num):
     assert 15/3 == x_num, 'The result is not as expected'
 
 
-@pytest.mark.usefixtures(name='this_test')
+@pytest.mark.usefixtures('this_test')
 @pytest.mark.hard
 @pytest.mark.parametrize('a_num, b_num', [[1, 2], [3, 4], [5, 6]])
 def test_6(a_num, b_num):
@@ -59,27 +59,27 @@ def test_6(a_num, b_num):
     assert a_num+b_num == result, 'The result is not as expected'
 
 
-@pytest.mark.usefixtures(name='this_test')
+@pytest.mark.usefixtures('this_test')
 @pytest.mark.simple
 def test_7():
     print('test_7')
     assert 4*4 == 15, 'The result is not as expected'
 
 
-@pytest.mark.usefixtures(name='this_test')
+@pytest.mark.usefixtures('this_test')
 @pytest.mark.parametrize('y_num', [56])
 def test_8(y_num):
     print('test_8')
     assert 7*8 == y_num, 'The result is not as expected'
 
 
-@pytest.mark.usefixtures(name='this_test')
+@pytest.mark.usefixtures('this_test')
 def test_9():
     print('test_9')
     assert 100/2 == 50, 'The result is not as expected'
 
 
-@pytest.mark.usefixtures(name='this_test')
+@pytest.mark.usefixtures('this_test')
 def test_10():
     print('test_10')
     assert 5+5 == 11, 'The result is not as expected'
