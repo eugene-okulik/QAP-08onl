022d2696bd0cf4ef61e4f56f19c00d7213bc9b09
eugeny.okulik <tiburon854@gmail.com>
2022-09-01 20:49:51 +0300

    json_reader updates

--- homework/eugeny_okulik/pom/tests/read_json.py
+++ homework/eugeny_okulik/pom/tests/read_json.py
@@ -2,8 +2,8 @@ import json
 
 
 with open('test_data/data.json', 'r', encoding="utf8") as json_file:
-    content = json_file.read()
-parsed_content = json.loads(content)
-print(content)
+    # content = json_file.read()
+    parsed_content = json.load(json_file)
+# print(content)
 print(parsed_content)
 print(parsed_content['children'][1])
