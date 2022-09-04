19223705af7fe5ccb015e679c9eb05cbb87a5568
Anastasiya Botukh <Aisatsana24@yandex.ru>
2022-09-04 20:23:38 +0300

    add encoding

--- homework/Anastasiya_Botukh/Homework_25/read_json_ht.py
+++ homework/Anastasiya_Botukh/Homework_25/read_json_ht.py
@@ -2,7 +2,7 @@ import json
 
 
 def user_info(files):
-    with open(f'json_data/{files}', 'r') as json_file:
+    with open(f'json_data/{files}', 'r', encoding="utf-8") as json_file:
         info = json_file.read()
         # print(info)
     analysed_info = json.loads(info)
