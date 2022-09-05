7ea02a19caaed340a654b05ee05ef92e8178e844
stassia92 <stassia92@mail.ru>
2022-09-05 12:13:16 +0300

    hw25 fixed

--- homework/Anastacia_Kuznetcova/Homework25/homework25.py
+++ homework/Anastacia_Kuznetcova/Homework25/homework25.py
@@ -5,10 +5,11 @@ def users_text(users):
     with open(f'json_data/{users}', encoding="utf8") as json_file:
         data = json_file.read()
         res = json.loads(data)
+        kids = ', '.join(res['children'])
         own_things = str(res['has']).replace(':', '').replace('{', '') \
             .replace('}', '').replace("'", "")
     print(f"User name is {res['name']}. He lives in {res['city']}. "
-          f"He has {len(res['children'])} kid(s). "
+          f"He has {len(res['children'])} kid(s) : {kids}. "
           f"He owns {own_things}.")
 
 
