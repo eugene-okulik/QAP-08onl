150ae89938825f60c33afbc93ff24c330f2daae4
stassia92 <106346132+stassia92@users.noreply.github.com>
2022-09-05 18:41:49 +0300

    Update homework25.py

--- homework/Anastacia_Kuznetcova/Homework25/homework25.py
+++ homework/Anastacia_Kuznetcova/Homework25/homework25.py
@@ -9,7 +9,7 @@ def users_text(users):
         own_things = str(res['has']).replace(':', '').replace('{', '') \
             .replace('}', '').replace("'", "")
     print(f"User name is {res['name']}. He lives in {res['city']}. "
-          f"He has {len(res['children'])} kid(s) : {kids}. "
+          f"He has {len(res['children'])} kid(s): {kids}. "
           f"He owns {own_things}.")
 
 
