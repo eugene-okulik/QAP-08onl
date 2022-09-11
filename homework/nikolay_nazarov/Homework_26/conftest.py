d13301f47e5487ff37e9fb946655e7626846f846
Nikolay <nikolaynazarovvv216@gmail.com>
2022-09-11 15:31:55 +0300

    Homework 26 done

--- homework/nikolay_nazarov/Homework_26/conftest.py
+++ homework/nikolay_nazarov/Homework_26/conftest.py
@@ -4,11 +4,13 @@ import pytest
 import requests
 
 DOMAIN = 'http://167.172.172.115:52353'
+MEME_ID_CONF = None
 
 
 @pytest.fixture(scope='session')
 def domain():
     yield DOMAIN
+
 
 @pytest.fixture(scope='session')
 def token(domain):
@@ -25,7 +27,51 @@ def token(domain):
     token = response["token"]
     yield token
 
-@pytest.fixture(scope='session')
-def token(domain):
-    yield
 
+@pytest.fixture(scope='session')
+def clean_up(domain, token):
+    yield
+    global MEME_ID_CONF
+    url = f'{domain}/meme/{MEME_ID_CONF}'
+    payload = {}
+    headers = {
+        'Authorization': f'{token}'
+    }
+
+    requests.request("DELETE", url, headers=headers, data=payload)
+
+
+@pytest.fixture(scope='session')
+def pre_condition_meme(domain, token):
+    url = f'{domain}/meme'
+    payload = json.dumps({
+        "text": "cat_text",
+        "url": "cat_url",
+        "tags": [
+            "cat_tag_1",
+            "cat_tag_2",
+            "cat_tag_3"
+        ],
+        "info": {
+            "cat_info_1": "cat_info_1_value",
+            "cat_info_2": "cat_info_2_value",
+            "cat_info_3": "cat_info_3_value"
+        }
+    })
+    headers = {
+        'Authorization': f'{token}',
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload).json()
+    pre_condition_meme_id = response["id"]
+    pre_condition_meme = {
+        'meme_id': pre_condition_meme_id,
+        'meme_text': f"{response['text']}",
+    }
+    yield pre_condition_meme
+
+
+def meme_id_to_conf(meme_id):
+    global MEME_ID_CONF
+    MEME_ID_CONF = meme_id
--- homework/nikolay_nazarov/Homework_26/task_1.py
+++ homework/nikolay_nazarov/Homework_26/task_1.py
@@ -1,8 +1,10 @@
-import requests
+import random
 import json
+import requests
+from conftest import meme_id_to_conf
 
 
-def test_meme_creation(domain, token):
+def test_meme_creation(domain, token, clean_up):
     # Создание мема
     url = f'{domain}/meme'
     payload = json.dumps({
@@ -33,12 +35,52 @@ def test_meme_creation(domain, token):
     headers = {
         'Authorization': f'{token}'
     }
-
     response = requests.request("GET", url, headers=headers, data=payload)
-    print(response.json()["id"])
+    meme_id_to_conf(meme_id)
+
     assert response.status_code == 200, "Статус-код не равен 200, тест провален"
+
+
+def test_meme_change(domain, token, clean_up, pre_condition_meme):
+    # Изменить мем
+    url = f"{domain}/meme/{pre_condition_meme['meme_id']}"
+    payload = json.dumps({
+        "id": pre_condition_meme['meme_id'],
+        "text": f"cat_text1{random.randint(1, 500)}",
+        "url": "cat_url",
+        "tags": [
+            "cat_tag_1",
+            "cat_tag_2",
+            "cat_tag_3"
+        ],
+        "info": {
+            "cat_info_1": "cat_info_1_value",
+            "cat_info_2": "cat_info_2_value",
+            "cat_info_3": "cat_info_3_value"
+        }
+    })
+    headers = {
+        'Authorization': f'{token}',
+        'Content-Type': 'application/json'
+    }
+    response_change_text = \
+        requests.request("PUT", url, headers=headers, data=payload).json()["text"]
+    # Получить мем
+    url = f"{domain}/meme/{pre_condition_meme['meme_id']}"
+
+    payload = {}
+    headers = {
+        'Authorization': f'{token}'
+    }
+
+    response_get_text = requests.request("GET", url, headers=headers, data=payload).json()["text"]
+    meme_id_to_conf(pre_condition_meme['meme_id'])
+    assert response_change_text == response_get_text
+
+
+def test_meme_deletion(domain, token, pre_condition_meme):
     # Удаление мема
-    url = f'{domain}/meme/{meme_id}'
+    url = f"{domain}/meme/{pre_condition_meme['meme_id']}"
 
     payload = {}
     headers = {
@@ -46,5 +88,31 @@ def test_meme_creation(domain, token):
     }
 
     response = requests.request("DELETE", url, headers=headers, data=payload)
+    # Получить мем
+    url = f"{domain}/meme/{pre_condition_meme['meme_id']}"
+    payload = {}
+    headers = {
+        'Authorization': f'{token}'
+    }
 
-    print(response.text)
+    response = requests.request("GET", url, headers=headers, data=payload)
+    assert response.status_code == 404
+
+
+def test_meme_get_all(domain, token):
+    # Получить все мемы
+    url = f"{domain}/meme"
+
+    payload = {}
+    headers = {
+        'Authorization': f'{token}'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload).json()
+    for value in response.values():
+        count_tags_fun = 0
+        for tags in value:
+            for tag in tags["tags"]:
+                if tag == 'fun':
+                    count_tags_fun += 1
+        assert count_tags_fun >= 1
