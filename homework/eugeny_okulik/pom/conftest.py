b77251d614d70fc7cd3cff95c8f6f24f278b528c
eugeny.okulik <tiburon854@gmail.com>
2022-09-12 21:32:17 +0300

    ho sandbox added

--- homework/eugeny_okulik/pom/conftest.py
+++ homework/eugeny_okulik/pom/conftest.py
@@ -10,6 +10,7 @@ def driver(browser_option):
     options = Options()
     # options.add_argument('window-size=1920,1080')
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
     # if browser_option == 'ff':
     if settings.BROWSER_NAME == 'ff':
         with allure.step('Run Firefox'):
