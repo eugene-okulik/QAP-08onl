3ad5720fba408423222521d43ce146ab5ed92375
SiarheiZhachek <SiarheiZhachek@yandex.ru>
2022-09-12 18:32:08 +0300

    sergey_zhachek_homework_26_fixed

--- homework/siarhei_zhachek/homework_26/conftest.py
+++ homework/siarhei_zhachek/homework_26/conftest.py
@@ -1,4 +1,6 @@
 import pytest
+import json
+import requests
 
 
 DOMAIN = "http://167.172.172.115:52353"
@@ -7,3 +9,56 @@ DOMAIN = "http://167.172.172.115:52353"
 @pytest.fixture(scope='function')
 def domain():
     yield DOMAIN
+
+
+@pytest.fixture(scope='function')
+def token():
+    with open('token.txt', 'r', encoding='UTF-8') as token_file:
+        token = token_file.read()
+    response = requests.request('GET', f'{DOMAIN}/authorize/{token}')
+    if response.status_code == 200:
+        pass
+    else:
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        data = json.dumps(
+            {
+                'name': 'Sergey_z'
+            }
+        )
+        response = requests.request('POST', f'{DOMAIN}/authorize', headers=headers, data=data).json()
+        with open('token.txt', 'w', encoding='UTF-8') as token_f:
+            token_f.write(response['token'])
+        token_f.close()
+    with open('token.txt', 'r', encoding='UTF-8') as token_file:
+        token = token_file.read()
+    yield token
+    token_file.close()
+
+
+@pytest.fixture(scope='function')
+def meme_text():
+    with open(f'data_json/my_meme.json', 'r', encoding='UTF-8') as meme_file:
+        new_meme = meme_file.read()
+    new_meme = json.loads(new_meme)
+    yield new_meme
+    meme_file.close()
+
+
+@pytest.fixture(scope='function')
+def changing_meme():
+    with open(f'data_json/changing_meme_text.json', 'r', encoding='UTF-8') as meme_change:
+        change_meme = meme_change.read()
+    new_meme = json.loads(change_meme)
+    yield new_meme
+    meme_change.close()
+
+
+@pytest.fixture(scope='function')
+def deleted_meme():
+    with open(f'data_json/text_for_delete_meme.json', 'r', encoding='UTF-8') as meme_delete:
+        delete_meme = meme_delete.read()
+    meme = json.loads(delete_meme)
+    yield meme
+    meme_delete.close()
--- homework/siarhei_zhachek/homework_26/test_meme.py
+++ homework/siarhei_zhachek/homework_26/test_meme.py
@@ -2,97 +2,32 @@ import json
 import requests
 
 
-with open('token.txt', 'r', encoding='UTF-8') as token_file:
-    token = token_file.read()
-
-
-def test_a_meme_has_been_created(domain):
+def test_a_meme_has_been_created(domain, meme_text, token):
     headers = {
         'Content-Type': 'application/json',
         'Authorization': token
     }
-    data = json.dumps({
-        "text": "What you will see is called a meme.",
-        "url": "https://jrnlst.ru/sites/default/files/covers/cover_6.jpg",
-        "tags": [
-            "fun",
-            "meme",
-            "Sergey Druzhko"
-         ],
-        "info":
-            {
-                "objects":
-                    ["picture",
-                        "text"],
-                "size":
-                {
-                    "width": "737",
-                    "height": "1288"
-                    }
-            },
-    })
+    data = json.dumps(meme_text)
     response = requests.request('POST', f'{domain}/meme', headers=headers, data=data).json()
     assert response['text'] == 'What you will see is called a meme.'
 
 
-def test_changing_the_meme(domain):
+def test_changing_the_meme(domain, changing_meme, token):
     headers = {
         'Content-Type': 'application/json',
         'Authorization': token
     }
-    data = json.dumps({
-        "id": 12,
-        "text": "What you will see is called a meme, cool memes!!!.",
-        "url": "https://jrnlst.ru/sites/default/files/covers/cover_6.jpg",
-        "tags": [
-            "fun",
-            "meme",
-            "Sergey Druzhko",
-            "bald man"
-         ],
-        "info":
-            {
-                "objects":
-                    ["picture - jpg",
-                        "text_rus"],
-                "size":
-                {
-                    "width": "737",
-                    "height": "1288"
-                    }
-            },
-    })
+    data = json.dumps(changing_meme)
     response = requests.request('PUT', f'{domain}/meme/12', headers=headers, data=data).json()
     assert response["text"] == 'What you will see is called a meme, cool memes!!!.'
 
 
-def test_delete_mem(domain):
+def test_delete_mem(domain, deleted_meme, token):
     headers = {
         'Content-Type': 'application/json',
         'Authorization': token
     }
-    data = json.dumps({
-        "text": "my_text",
-        "url": "my_url.com",
-        "tags": [
-            "meme",
-            "bald man"
-         ],
-        "info":
-            {
-                "objects":
-                    [
-                        "picture",
-                        "text"
-                    ],
-                "colors":
-                    [
-                        "green",
-                        "black",
-                        "purple"
-                    ]
-            }
-    })
+    data = json.dumps(deleted_meme)
     response = requests.request(
         'POST',
         f'{domain}/meme',
@@ -105,10 +40,10 @@ def test_delete_mem(domain):
     assert response.status_code == 404
 
 
-def test_existence_of_a_meme_with_the_fun_tag(domain):
+def test_existence_of_a_meme_with_the_fun_tag(domain, token):
     headers = {
         'Content-Type': 'application/json',
-        'Authorization': token,
+        'Authorization': token
     }
     response = requests.request('GET', f'{domain}/meme', headers=headers).json()
     for value in dict(response).values():
