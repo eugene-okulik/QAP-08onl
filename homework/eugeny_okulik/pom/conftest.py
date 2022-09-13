c578b4086d23a6a33b329125f22a92e446466aa7
eugeny.okulik <tiburon854@gmail.com>
2022-09-13 08:22:52 +0300

    added options

--- homework/eugeny_okulik/pom/conftest.py
+++ homework/eugeny_okulik/pom/conftest.py
@@ -9,7 +9,11 @@ import settings
 def driver(browser_option):
     options = Options()
     # options.add_argument('window-size=1920,1080')
-    options.add_argument("--headless")
+    options.add_argument("start-maximized")
+    options.add_argument("disable-infobars")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
     # if browser_option == 'ff':
     if settings.BROWSER_NAME == 'ff':
