49604ac59ee525d156912d58230679a86ac7520c
Anastasiya Botukh <Aisatsana24@yandex.ru>
2022-09-14 19:57:26 +0300

    upper_case

--- homework/Anastasiya_Botukh/Homework_27/db_ht.py
+++ homework/Anastasiya_Botukh/Homework_27/db_ht.py
@@ -11,34 +11,34 @@ db = mysql.connect(
 
 cursor = db.cursor(dictionary=True)
 
-new_group = 'INSERT INTO `groups` (title, start_date, end_date) ' \
+NEW_GROUP = 'INSERT INTO `groups` (title, start_date, end_date) ' \
             'VALUES (%s, %s, %s)'
 values_new_group = ('JUNGLES', '2022-02-02', '2022-09-10')
-cursor.execute(new_group, values_new_group)
+cursor.execute(NEW_GROUP, values_new_group)
 id_group = cursor.lastrowid
 db.commit()
 
-new_student = 'INSERT INTO students (name, second_name, group_id) ' \
+NEW_STUDENT = 'INSERT INTO students (name, second_name, group_id) ' \
             'VALUES (%s, %s, %s)'
 values_student = ('Lion', 'King', id_group)
-cursor.execute(new_student, values_student)
+cursor.execute(NEW_STUDENT, values_student)
 id_student = cursor.lastrowid
 db.commit()
 
-new_book_1 = 'INSERT INTO books (title, taken_by_student_id, return_date) ' \
+NEW_BOOK_1 = 'INSERT INTO books (title, taken_by_student_id, return_date) ' \
             'VALUES (%s, %s, %s)'
 value_book_1 = [
     ('"Duma-ki"', id_student, '2022-09-17')
 ]
-cursor.execute(new_book_1, value_book_1)
+cursor.execute(NEW_BOOK_1, value_book_1)
 db.commit()
 
-new_book_2 = 'INSERT INTO books (title, taken_by_student_id, return_date) ' \
+NEW_BOOK_2 = 'INSERT INTO books (title, taken_by_student_id, return_date) ' \
             'VALUES (%s, %s, %s)'
 value_book_2 = [
     ('"Kydjo"', id_student, '2022-10-01')
 ]
-cursor.execute(new_book_2, value_book_2)
+cursor.execute(NEW_BOOK_2, value_book_2)
 db.commit()
 
 cursor.execute(f'''
