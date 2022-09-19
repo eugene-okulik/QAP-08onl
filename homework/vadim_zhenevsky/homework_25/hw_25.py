ba8dfc3d9c776f8340d75f921dd4ce94945f0ca1
Vadim Zhenevsky <zhenevskyvadim@gmail.com>
2022-09-19 18:35:45 +0300

    исправил замечание

--- homework/vadim_zhenevsky/homework_25/hw_25.py
+++ homework/vadim_zhenevsky/homework_25/hw_25.py
@@ -1,17 +1,18 @@
 import json
 
 
-def users_info(users):
-    with open(f'data_json/{users}', encoding="utf8") as json_file:
-        content = json_file.read()
-        parsed_content = json.loads(content)
-        things_owned = str(parsed_content['has']).replace(':', '').replace('{', '') \
+def users_text(users):
+    with open(f'json_data/{users}', encoding="utf8") as json_file:
+        data = json_file.read()
+        res = json.loads(data)
+        kids = ', '.join(res['children'])
+        own_things = str(res['has']).replace(':', '').replace('{', '') \
             .replace('}', '').replace("'", "")
-    print(f"User name is {parsed_content['name']}. He lives in {parsed_content['city']}. "
-          f"He has {len(parsed_content['children'])} kids. "
-          f"He owns a {things_owned}.")
+    print(f"User name is {res['name']}. He lives in {res['city']}. "
+          f"He has {len(res['children'])} kid(s): {kids}. "
+          f"He owns {own_things}.")
 
 
-users_info('user1.json')
-users_info('user2.json')
-users_info('user3.json')
+users_text('user1.json')
+users_text('user2.json')
+users_text('user3.json')
